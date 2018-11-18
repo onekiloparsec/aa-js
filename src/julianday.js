e17c7473a389e56265fddb6fd9897353e589d0e3
Cédric Foellmi <cedric@onekilopars.ec>
2018-11-18 17:50:11 +0100

    extracting method

--- src/julianday.js
+++ src/julianday.js
@@ -7,6 +7,20 @@ const J1970 = 2440588
 const J2000 = 2451545
 const dayMs = 1000 * 60 * 60 * 24
 
+function getLocalSiderealTime (julianDay, longitude) {
+  // Equ 12.1
+  const t = (julianDay - 2451545) / 36525
+
+  // Greenwhich SiderealTime in degrees! Equ. 12.4 of AA, p. 88
+  let gmst = 280.46061837 + 360.98564736629 * (julianDay - 2451545) + 0.000387933 * t * t - t * t * t / 38710000
+
+  while (gmst < 0) {
+    gmst = gmst + 360
+  }
+
+  return Math.fmod((gmst + longitude) * constants.DEGREES_TO_HOURS + 24, 24)
+}
+
 class JulianDay {
   constructor (...args) {
     if (args.length === 0) {
@@ -28,22 +42,17 @@ class JulianDay {
   }
 
   getLocalSiderealTime (longitude) {
-    // Equ 12.1
-    const t = (this.value - 2451545) / 36525
+    return getLocalSiderealTime(this.value, longitude)
+  }
 
-    // Greenwhich SiderealTime in degrees! Equ. 12.4 of AA, p. 88
-    let gmst = 280.46061837 + 360.98564736629 * (this.value - 2451545) + 0.000387933 * t * t - t * t * t / 38710000
-
-    while (gmst < 0) {
-      gmst = gmst + 360
-    }
-
-    return Math.fmod((gmst + longitude) * constants.DEGREES_TO_HOURS + 24, 24)
+  getMidnightJulianDay () {
+    return new JulianDay(Math.floor(this.value - 0.5) + 0.5)
   }
 }
 
 export default {
   J1970,
   J2000,
+  getLocalSiderealTime,
   JulianDay
 }
