f7e09f4e6bc80f8f3590410b7285af9f0a265347
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-02-29 12:22:10 +0100

    Adding age at redshift

--- src/cosmology.js
+++ src/cosmology.js
@@ -116,8 +116,8 @@ function getComovingVolume (WK, DCMR) {
   return ratio * DCMR * DCMR * DCMR / 3
 }
 
-function getUniverseAge (H0, WM, WV, z = 0) {
-  let az = 1.0 / (1 + z)
+function getUniverseAge (H0, WM, WV) {
+  let az = 1.0
   let WR = getOmegaR(H0)
   let WK = getOmegaK(H0, WM, WV)
 
@@ -131,12 +131,49 @@ function getUniverseAge (H0, WM, WV, z = 0) {
   return age * (Tyr / H0) / 1000
 }
 
+function getUniverseAgeAtRedshift (H0, WM, WV, z) {
+  let az = 1.0 / (1 + z)
+  let WR = getOmegaR(H0)
+  let WK = getOmegaK(H0, WM, WV)
+
+  let age = 0
+  for (let i = 0; i < INTEGRAL_POINTS_NUMBER; i++) {
+    const a = az * (i + 0.5) / INTEGRAL_POINTS_NUMBER
+    const adot = Math.sqrt(WK + (WM / a) + (WR / (a * a)) + (WV * a * a))
+    age = age + 1 / adot
+  }
+
+  let zage = az * age / INTEGRAL_POINTS_NUMBER
+
+  // correction for annihilations of particles not present now like e+/e-
+  // added 13-Aug-03 based on T_vs_t.f
+  var lpz = Math.log((1 + 1.0 * z)) / Math.log(10.0)
+  var dzage = 0
+  if (lpz > 7.500) dzage = 0.002 * (lpz - 7.500)
+  if (lpz > 8.000) dzage = 0.014 * (lpz - 8.000) + 0.001
+  if (lpz > 8.500) dzage = 0.040 * (lpz - 8.500) + 0.008
+  if (lpz > 9.000) dzage = 0.020 * (lpz - 9.000) + 0.028
+  if (lpz > 9.500) dzage = 0.019 * (lpz - 9.500) + 0.039
+  if (lpz > 10.000) dzage = 0.048
+  if (lpz > 10.775) dzage = 0.035 * (lpz - 10.775) + 0.048
+  if (lpz > 11.851) dzage = 0.069 * (lpz - 11.851) + 0.086
+  if (lpz > 12.258) dzage = 0.461 * (lpz - 12.258) + 0.114
+  if (lpz > 12.382) dzage = 0.024 * (lpz - 12.382) + 0.171
+  if (lpz > 13.055) dzage = 0.013 * (lpz - 13.055) + 0.188
+  if (lpz > 14.081) dzage = 0.013 * (lpz - 14.081) + 0.201
+  if (lpz > 15.107) dzage = 0.214
+
+  // Gyr
+  return (Tyr / H0) * zage * Math.pow(10.0, dzage)
+}
+
 export default {
   getOmegaR,
   getOmegaK,
   getTangentialComovingDistance,
   getComovingVolume,
-  getUniverseAge
+  getUniverseAge,
+  getUniverseAgeAtRedshift
 }
 
 // function getUniverseAge
--- tests/cosmology.test.js
+++ tests/cosmology.test.js
@@ -1,7 +1,18 @@
 import cosmology from '../src/cosmology'
 
 test('get age of the Universe', () => {
-  expect(cosmology.getUniverseAge(59.6, 0.286, 0.714)).toBeCloseTo(16.021, 0.001)
-  expect(cosmology.getUniverseAge(69.6, 0.286, 0.714)).toBeCloseTo(13.721, 0.001)
-  expect(cosmology.getUniverseAge(79.6, 0.286, 0.714)).toBeCloseTo(11.998, 0.001)
+  expect(cosmology.getUniverseAge(59.6, 0.286, 0.714)).toBeCloseTo(16.021, 0.0001)
+  expect(cosmology.getUniverseAge(69.6, 0.286, 0.714)).toBeCloseTo(13.721, 0.0001)
+  expect(cosmology.getUniverseAge(79.6, 0.286, 0.714)).toBeCloseTo(11.998, 0.0001)
 })
+
+test('get age of the Universe at redshift', () => {
+  expect(cosmology.getUniverseAgeAtRedshift(69.6, 0.286, 0.714, 0.1)).toBeCloseTo(12.411, 0.0001)
+  expect(cosmology.getUniverseAgeAtRedshift(69.6, 0.286, 0.714, 1)).toBeCloseTo(5.903, 0.0001)
+  expect(cosmology.getUniverseAgeAtRedshift(69.6, 0.286, 0.714, 2)).toBeCloseTo(3.316, 0.0001)
+  expect(cosmology.getUniverseAgeAtRedshift(69.6, 0.286, 0.714, 3)).toBeCloseTo(2.171, 0.0001)
+  expect(cosmology.getUniverseAgeAtRedshift(69.6, 0.286, 0.714, 10)).toBeCloseTo(0.478, 0.0001)
+  expect(cosmology.getUniverseAgeAtRedshift(69.6, 0.286, 0.714, 100)).toBeCloseTo(16.633 / 1000, 0.0001)
+  expect(cosmology.getUniverseAgeAtRedshift(69.6, 0.286, 0.714, 1000)).toBeCloseTo(0.434 / 1000, 0.0001)
+})
+
