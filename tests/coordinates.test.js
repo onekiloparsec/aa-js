123bd5be2ba250d83fbe9fbac7610bf2f42863b7
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-10-10 22:31:05 +0200

    Making tests pass again
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- tests/coordinates.test.js
+++ tests/coordinates.test.js
@@ -2,53 +2,39 @@ import coordinates from '../src/coordinates'
 import sexagesimal from '../src/sexagesimal'
 import julianday from '../src/julianday'
 
-test('get altitude', () => {
-  expect(coordinates.getHorizontalAltitude({})).toBe(-1)
-  expect(coordinates.getHorizontalAltitude({ julianDayValue: 24550000 })).toBe(-1)
-  expect(coordinates.getHorizontalAltitude({ julianDayValue: null, targetCoords: {}, siteCoords: {} })).toBe(-1)
-})
-
 test('parallactic angle before meridian', () => {
   const utcDate = new Date(Date.UTC(2017, 5, 14, 2, 0, 0.0))
-  let julianDayValue = julianday.getJulianDay(utcDate)
+  let jd = julianday.getJulianDay(utcDate)
 
   // gro_j1655_40, see below
-  let skyCoords = {
-    rightAscension: sexagesimal.getDecimal(16, 54, 0.14), // in hours
-    declination: sexagesimal.getDecimal(39, 50, 44.9, false)
-  }
+  let ra = sexagesimal.getDecimal(16, 54, 0.14) // in hours
+  let dec = sexagesimal.getDecimal(39, 50, 44.9, false)
 
   // http://www.ls.eso.org/lasilla/Telescopes/2p2/D1p5M/
-  let siteCoords = {
-    longitude: sexagesimal.getDecimal(70, 44, 7.662, false), // east-positive
-    latitude: sexagesimal.getDecimal(29, 15, 14.235, false)
-  }
+  let lng = sexagesimal.getDecimal(70, 44, 7.662, false) // east-positive
+  let lat = sexagesimal.getDecimal(29, 15, 14.235, false)
 
   // See https://www.eso.org/sci/observing/tools/calendar/ParAng.html to check values.
   let refAngle = -78.1
 
-  expect(coordinates.getParallacticAngle({ julianDayValue, skyCoords, siteCoords })).toBeCloseTo(refAngle, 0)
+  expect(coordinates.getParallacticAngle(jd, ra, dec, lat, lng)).toBeCloseTo(refAngle, 0)
 })
 
 test('parallactic angle after meridian', () => {
   const utcDate = new Date(Date.UTC(2017, 5, 14, 6, 0, 0.0))
-  let julianDayValue = julianday.getJulianDay(utcDate)
+  let jd = julianday.getJulianDay(utcDate)
 
   // gro_j1655_40, see below
-  let skyCoords = {
-    rightAscension: sexagesimal.getDecimal(16, 54, 0.14), // in hours
-    declination: sexagesimal.getDecimal(39, 50, 44.9, false)
-  }
+  let ra = sexagesimal.getDecimal(16, 54, 0.14) // in hours
+  let dec = sexagesimal.getDecimal(39, 50, 44.9, false)
 
   // http://www.ls.eso.org/lasilla/Telescopes/2p2/D1p5M/
-  let siteCoords = {
-    longitude: sexagesimal.getDecimal(70, 44, 7.662, false), // east-positive
-    latitude: sexagesimal.getDecimal(29, 15, 14.235, false)
-  }
+  let lng = sexagesimal.getDecimal(70, 44, 7.662, false) // east-positive
+  let lat = sexagesimal.getDecimal(29, 15, 14.235, false)
 
   // Slightly adjusted values. Ref might not be 100% accurate...
   // Looking for improved reference values...
   let refAngle = 74.4
 
-  expect(coordinates.getParallacticAngle({ julianDayValue, skyCoords, siteCoords })).toBeCloseTo(refAngle, 0)
+  expect(coordinates.getParallacticAngle(jd, ra, dec, lat, lng)).toBeCloseTo(refAngle, 0)
 })
--- tests/moon.test.js
+++ tests/moon.test.js
@@ -1,6 +1,6 @@
 import moon from '../src/moon'
 import julianday from '../src/julianday'
-import { DEGREES_TO_HOURS } from '../src/constants'
+import { DEG2H } from '../src/constants'
 
 test('get moon mean longitude', () => {
   expect(moon.getMeanLongitude(245123456)).toBe(182.125250)
@@ -14,7 +14,7 @@ test('get moon equatorial coordinates', () => {
   const UTCDate = new Date(Date.UTC(1992, 3, 12))
   const jd = julianday.getJulianDay(UTCDate)
   const equ = moon.getEquatorialCoordinates(jd)
-  expect(equ.rightAscension).toBeCloseTo(134.688470 * DEGREES_TO_HOURS)
+  expect(equ.rightAscension).toBeCloseTo(134.688470 * DEG2H)
   expect(equ.declination).toBeCloseTo(13.768368)
 })
 
