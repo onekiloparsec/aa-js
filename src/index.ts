81a89b9974059522101385e7deb83c0745070558
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-10-23 11:57:48 +0200

    Fixing index
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,25 @@
 import * as coordinates from './coordinates'
+import * as cosmology from './cosmology'
+import * as distances from './distances'
 import * as earth from './earth'
 import * as julianday from './julianday'
+import * as mars from './mars'
 import * as moon from './moon'
-import * as sun from './sun'
 import * as nutation from './nutation'
-import * as transits from './transits'
-import * as distances from './distances'
-import * as cosmology from './cosmology'
+import * as sun from './sun'
 import * as times from './times'
+import * as transits from './transits'
 
 export default {
   coordinates,
+  cosmology,
+  distances,
   earth,
   julianday,
+  mars,
   moon,
-  sun,
   nutation,
+  sun,
+  times,
   transits,
-  distances,
-  cosmology,
-  times
 }
