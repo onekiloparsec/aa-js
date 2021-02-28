aa4c00a9f5570d94e6bcde5cc59fff4e204804c7
Cédric Foellmi <cedric@onekiloparsec.dev>
2021-02-28 16:04:01 +0100

    Saturn!
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

---
+++ src/saturn/index.ts
@@ -0,0 +1,10 @@
+import {  getEclipticLatitude, getEclipticLongitude, getRadiusVector } from './coordinates'
+import { getApparentGeocentricDistance, getApparentGeocentricEquatorialCoordinates } from './planetaryDetails'
+
+export {
+  getEclipticLongitude,
+  getEclipticLatitude,
+  getRadiusVector,
+  getApparentGeocentricDistance,
+  getApparentGeocentricEquatorialCoordinates
+}
