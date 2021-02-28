8c769489b92c570c3406323d16359a41eca3fb96
Cédric Foellmi <cedric@onekiloparsec.dev>
2021-02-28 16:25:46 +0100

    Import all
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,37 @@ import * as coordinates from './coordinates'
 import * as cosmology from './cosmology'
 import * as distances from './distances'
 import * as julianday from './julianday'
-import * as mars from './mars'
-import { earth, moon } from './earth'
 import * as nutation from './nutation'
-import * as sun from './sun'
 import * as times from './times'
 import * as transits from './transits'
+
+import * as sun from './sun'
+import * as mercury from './mercury'
+import * as venus from './venus'
+import { earth, moon } from './earth'
+import * as mars from './mars'
+import * as jupiter from './jupiter'
+import * as saturn from './saturn'
+import * as neptune from './neptune'
+import * as pluto from './pluto'
 
 export default {
   constants,
   coordinates,
   cosmology,
   distances,
-  earth,
   julianday,
-  mars,
-  moon,
   nutation,
-  sun,
   times,
   transits,
+  sun,
+  mercury,
+  venus,
+  earth,
+  moon,
+  mars,
+  jupiter,
+  saturn,
+  neptune,
+  pluto
 }
