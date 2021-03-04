48af7fcfa0d004b2e71b8c66d6d38f4b3c422bd7
Cédric Foellmi <cedric@onekiloparsec.dev>
2021-03-04 08:08:19 +0100

    Adding details of Mercury
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- src/mercury/coordinates.ts
+++ src/mercury/coordinates.ts
@@ -1,5 +1,8 @@
 import { AstronomicalUnit, Degree, JulianDay, RAD2DEG } from '../constants'
 import { MapTo0To360Range, MapToMinus90To90Range } from '../utils'
+import { EclipticCoordinates, EquatorialCoordinates } from '../coordinates'
+import { getEllipticalDetails } from '../elliptical'
+
 import {
   g_B0MercuryCoefficients,
   g_B1MercuryCoefficients,
@@ -17,7 +20,6 @@ import {
   g_R2MercuryCoefficients,
   g_R3MercuryCoefficients
 } from './coefficients'
-import { EclipticCoordinates } from '../coordinates'
 
 const cos = Math.cos
 
@@ -140,4 +142,9 @@ export function getEclipticCoordinates(JD: JulianDay): EclipticCoordinates {
     longitude: getEclipticLongitude(JD),
     latitude: getEclipticLatitude(JD)
   }
+}
+
+export function getEquatorialCoordinates(jd: JulianDay): EquatorialCoordinates {
+  const details = getEllipticalDetails(jd, getEclipticLongitude, getEclipticLatitude, getRadiusVector)
+  return details.apparentGeocentricEquatorialCoordinates
 }
--- src/mercury/index.ts
+++ src/mercury/index.ts
@@ -1,10 +1,11 @@
-import { getEclipticCoordinates, getEclipticLatitude, getEclipticLongitude, getRadiusVector } from './coordinates'
+import { getEclipticCoordinates, getEclipticLatitude, getEclipticLongitude, getEquatorialCoordinates, getRadiusVector } from './coordinates'
 import { getPlanetaryDetails } from './planetaryDetails'
 
 export {
   getEclipticLongitude,
   getEclipticLatitude,
   getEclipticCoordinates,
+  getEquatorialCoordinates,
   getRadiusVector,
   getPlanetaryDetails
 }
