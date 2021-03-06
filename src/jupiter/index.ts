9f4a96d5ee78425c25cfa46b59cc94ba2200c382
Cédric Foellmi <cedric@onekiloparsec.dev>
2021-03-06 09:01:11 +0100

    Jupiter
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- src/jupiter/index.ts
+++ src/jupiter/index.ts
@@ -1,13 +1,25 @@
 import { getEclipticCoordinates, getEclipticLatitude, getEclipticLongitude, getRadiusVector } from './coordinates'
-import { getPlanetaryDetails } from './planetaryDetails'
 import { getCentralMeridianLongitudes, getPlanetocentricDeclinationOfTheEarth, getPlanetocentricDeclinationOfTheSun } from './jupiterDetails'
+import {
+  getEquatorialSemiDiameter,
+  getIlluminatedFraction,
+  getMagnitude,
+  getPhaseAngle,
+  getPlanetaryDetails,
+  getPolarSemiDiameter
+} from './planetaryDetails'
 
 export {
   getEclipticLongitude,
   getEclipticLatitude,
   getEclipticCoordinates,
   getRadiusVector,
+  getEquatorialSemiDiameter,
+  getIlluminatedFraction,
+  getMagnitude,
+  getPhaseAngle,
   getPlanetaryDetails,
+  getPolarSemiDiameter,
   getPlanetocentricDeclinationOfTheEarth,
   getPlanetocentricDeclinationOfTheSun,
   getCentralMeridianLongitudes
