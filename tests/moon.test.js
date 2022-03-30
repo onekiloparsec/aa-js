4ca0fb70b0c5d5536e8735d079f42ff0079cfd98
Cédric Foellmi <cedric@onekiloparsec.dev>
2022-03-30 08:42:07 +0200

    New moon tests
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- tests/moon.test.js
+++ tests/moon.test.js
@@ -10,6 +10,7 @@ test('get moon mean elongation', () => {
   expect(moon.getMeanElongation(245123456)).toBe(175.56631)
 })
 
+// See example 47.a, AA p 342.
 test('get moon equatorial coordinates', () => {
   const UTCDate = new Date(Date.UTC(1992, 3, 12))
   const jd = julianday.getJulianDay(UTCDate)
@@ -18,3 +19,18 @@ test('get moon equatorial coordinates', () => {
   expect(equ.declination).toBeCloseTo(13.768368)
 })
 
+// See example 48.a, AA p 347.
+test('get moon radius vector', () => {
+  const UTCDate = new Date(Date.UTC(1992, 3, 12))
+  const jd = julianday.getJulianDay(UTCDate)
+  const rv = moon.getRadiusVector(jd)
+  expect(rv).toBeCloseTo(368409.68, 1) // km, scnd param is number of digits checked.
+})
+
+// See example 48.a, AA p 347.
+test('get moon illumination fraction', () => {
+  const UTCDate = new Date(Date.UTC(1992, 3, 12))
+  const jd = julianday.getJulianDay(UTCDate)
+  const i = moon.getIlluminatedFraction(jd)
+  expect(i).toBeCloseTo(0.6786)
+})
