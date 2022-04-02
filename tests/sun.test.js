73cf552a4de9c91ecfc5648e06ddcb1b869a466b
Cédric Foellmi <cedric@onekiloparsec.dev>
2022-04-02 11:18:29 +0200

    Adding test
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- tests/sun.test.js
+++ tests/sun.test.js
@@ -1,5 +1,6 @@
 import * as sun from '../src/sun'
 import * as julianday from '../src/julianday'
+import { H2DEG } from '../src/constants'
 
 test('get sun geometric longitude mean equinox of the date (AA p.165)', () => {
   const UTCDate = new Date(Date.UTC(1992, 9, 13))
@@ -24,6 +25,14 @@ test('get sun apparent equatorial coordinates (AA p.165)', () => {
   const equ = sun.getApparentEquatorialCoordinates(jd)
   expect(equ.rightAscension).toBeCloseTo(13.225389, 3) // accuracy is bad, as in SwiftAA !
   expect(equ.declination).toBeCloseTo(-7.78507, 2) // accuracy is bad, as in SwiftAA !
+})
+
+test('get another sun apparent equatorial coordinates AA p.343', () => {
+  const UTCDate = new Date(Date.UTC(1992, 3, 12))
+  const jd = julianday.getJulianDay(UTCDate)
+  const equ = sun.getApparentEquatorialCoordinates(jd)
+  expect(equ.rightAscension * H2DEG).toBeCloseTo(20.6579, 3)
+  expect(equ.declination).toBeCloseTo(8.6964, 3)
 })
 
 test('get all sun events julian days', () => {
