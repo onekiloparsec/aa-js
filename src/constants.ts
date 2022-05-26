6ddbe3f1f27d5181bf94b9a84b1ff5849ae9c49d
Cédric Foellmi <cedric@onekiloparsec.dev>
2022-05-26 12:32:51 +0200

    Adding types to constants
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- src/constants.ts
+++ src/constants.ts
@@ -1,55 +1,56 @@
-export const J1970 = 2440588.0
-export const J2000 = 2451545.0
+import { Day, Degree, Hour, JulianDay, Kilogram, Kilometer, KilometerPerSecond, MiliSecond, Minute, Radian, Second } from 'aa.js'
 
-export const MJD_START = 2400000.5
-export const DAYMS = 1000 * 60 * 60 * 24
+export const J1970: JulianDay = 2440588.0
+export const J2000: JulianDay = 2451545.0
 
-export const DEG2RAD = 0.017453292519943295769236907684886
-export const RAD2DEG = 57.295779513082320876798154814105
-export const RAD2H = 3.8197186342054880584532103209403
-export const H2RAD = 0.26179938779914943653855361527329
-export const H2DEG = 15.0
-export const DEG2H = 0.06666666666666666666666666666666666
+export const MJD_START: JulianDay = 2400000.5
+export const DAYMS: MiliSecond = 1000 * 60 * 60 * 24
 
-export const SPEED_OF_LIGHT = 299792.458
-export const SPEED_OF_LIGHT_MS = 299792458.0
-export const SPEED_OF_LIGHT_APPROX_MS = 300000000.0
+export const DEG2RAD: Radian = 0.017453292519943295769236907684886
+export const RAD2DEG: Degree = 57.295779513082320876798154814105
+export const RAD2H: Hour = 3.8197186342054880584532103209403
+export const H2RAD: Radian = 0.26179938779914943653855361527329
+export const H2DEG: Degree = 15.0
+export const DEG2H: Hour = 0.06666666666666666666666666666666666
 
-export const SIDEREAL_OVER_SOLAR_RATE = 1.0027379093	// Sidereal / solar rate.
-export const AVERAGE_JULIAN_YEAR = 365.25		// See Observer's handbook (1999 - RAS of Canada).
-export const AVERAGE_GREGORIAN_YEAR = 365.2425		//
-export const AVERAGE_SIDEREAL_YEAR = 365.256363	// Fixed star to fixed star.
-export const AVERAGE_ANOMALISTIC_YEAR = 365.259635	// Perihelion to perihelion.
-export const AVERAGE_TROPICAL_YEAR = 365.242190	// Equinox to equinox.
-export const AVERAGE_ECLIPSE_YEAR = 346.620075	// Lunar mode to lunar mode.
+export const SPEED_OF_LIGHT: KilometerPerSecond = 299792.458
+export const SPEED_OF_LIGHT_APPROX = 300000.0
 
-export const ONE_YEAR_IN_SECONDS = 31556952.0	// AVERAGE_GREGORIAN_YEAR * 86400;
-export const ONE_MONTH_IN_SECONDS = 2635200.0	// 30.5 * 86400 || IMPRECISE!!!
-export const ONE_WEEK_IN_SECONDS = 604800.0		// 7*86400;
-export const ONE_DAY_IN_SECONDS = 86400.0
-export const ONE_DAY_IN_MINUTES = 1440.0
-export const ONE_DAY_IN_HOURS = 24.0
-export const ONE_HOUR_IN_SECONDS = 3600.0
-export const ONE_HOUR_IN_MINUTES = 60.0
-export const ONE_MINUTE_IN_SECONDS = 60.0
-export const HALF_DAY_IN_HOURS = 12.0
+export const SIDEREAL_OVER_SOLAR_RATE: Day = 1.0027379093	// Sidereal / solar rate.
+export const AVERAGE_JULIAN_YEAR: Day = 365.25		// See Observer's handbook (1999 - RAS of Canada).
+export const AVERAGE_GREGORIAN_YEAR: Day = 365.2425		//
+export const AVERAGE_SIDEREAL_YEAR: Day = 365.256363	// Fixed star to fixed star.
+export const AVERAGE_ANOMALISTIC_YEAR: Day = 365.259635	// Perihelion to perihelion.
+export const AVERAGE_TROPICAL_YEAR: Day = 365.242190	// Equinox to equinox.
+export const AVERAGE_ECLIPSE_YEAR: Day = 346.620075	// Lunar mode to lunar mode.
+
+export const ONE_YEAR_IN_SECONDS: Second = 31556952.0	// AVERAGE_GREGORIAN_YEAR * 86400;
+export const ONE_MONTH_IN_SECONDS: Second = 2635200.0	// 30.5 * 86400 || IMPRECISE!!!
+export const ONE_WEEK_IN_SECONDS: Second = 604800.0		// 7*86400;
+export const ONE_DAY_IN_SECONDS: Second = 86400.0
+export const ONE_DAY_IN_MINUTES: Second = 1440.0
+export const ONE_DAY_IN_HOURS: Hour = 24.0
+export const ONE_HOUR_IN_SECONDS: Second = 3600.0
+export const ONE_HOUR_IN_MINUTES: Minute = 60.0
+export const ONE_MINUTE_IN_SECONDS: Second = 60.0
+export const HALF_DAY_IN_HOURS: Hour = 12.0
 
 export const NOT_A_SCIENTIFIC_NUMBER = -999999999999999.0
 
-export const ECLIPTIC_OBLIQUITY_J2000_0 = 23.4392911	// In degrees, see p.92 of AA.
-export const ECLIPTIC_OBLIQUITY_B1950_0 = 23.4457889	// In degrees, see p.92 of AA.
+export const ECLIPTIC_OBLIQUITY_J2000_0: Degree = 23.4392911	// In degrees, see p.92 of AA.
+export const ECLIPTIC_OBLIQUITY_B1950_0: Degree = 23.4457889	// In degrees, see p.92 of AA.
 
-export const JULIAN_YEAR = 365.25		// See p.133 of AA.
-export const BESSELIAN_YEAR = 365.2421988	// See p.133 of AA.
-export const JULIAN_DAY_B1950_0 = 2433282.4235	// See p.133 of AA.
+export const JULIAN_YEAR: Day = 365.25		// See p.133 of AA.
+export const BESSELIAN_YEAR: Day = 365.2421988	// See p.133 of AA.
+export const JULIAN_DAY_B1950_0: JulianDay = 2433282.4235	// See p.133 of AA.
 
-export const GALACTIC_NORTH_POLE_ALPHA_B1950_0 = 192.25
-export const GALACTIC_NORTH_POLE_DELTA_B1950_0 = 27.4
+export const GALACTIC_NORTH_POLE_ALPHA_B1950_0: Degree = 192.25
+export const GALACTIC_NORTH_POLE_DELTA_B1950_0: Degree = 27.4
 
-export const EARTH_EQUATORIAL_RADIUS = 6378.14		// See p82 of AA.
+export const EARTH_EQUATORIAL_RADIUS: Kilometer = 6378.14		// See p82 of AA.
 export const EARTH_RADIUS_FLATTENING_FACTOR = 1. / 298.257	// See p82 of AA.
 
-export const ONE_UA_IN_KILOMETERS = 149597870.691
+export const ONE_UA_IN_KILOMETERS: Kilometer = 149597870.691
 /*
  http://neo.jpl.nasa.gov/glossary/au.html
  Definition: An Astronomical Unit is approximately the mean distance between the Earth and the Sun. It is a derived 
@@ -70,19 +71,19 @@ export const BOLTZMANN_CONSTANT = 1.3806488e-23 // Joule/Kelvin
 // http://nssdc.gsfc.nasa.gov/planetary/factsheet/
 // http://solarscience.msfc.nasa.gov
 
-export const MSUN = 1.98855e30 // kg;
-export const MJUP = 1.8990e27 // kg;
-export const MNEP = 1.0243e26 // kg;
-export const MEARTH = 5.9736e24 // kg;
+export const MSUN: Kilogram = 1.98855e30 // kg;
+export const MJUP: Kilogram = 1.8990e27 // kg;
+export const MNEP: Kilogram = 1.0243e26 // kg;
+export const MEARTH: Kilogram = 5.9736e24 // kg;
 
 export const ONE_MASS_OF_JUPITER_IN_NEPTUNE_MASS = 18.539490383676657
 export const ONE_MASS_OF_JUPITER_IN_EARTH_MASS = 317.8987545198875
 
 // EQUATORIAL RADII. See http://nssdc.gsfc.nasa.gov/planetary/factsheet/jupiterfact.html
-export const ONE_SOLAR_RADIUS_IN_KILOMETERS = 695990.0 // km;
-export const ONE_JUPITER_RADIUS_IN_KILOMETERS = 71492.0//
-export const ONE_NEPTUNE_RADIUS_IN_KILOMETERS = 24764.0//
-export const ONE_EARTH_RADIUS_IN_KILOMETERS = 6378.137//
+export const ONE_SOLAR_RADIUS_IN_KILOMETERS: Kilometer = 695990.0 // km;
+export const ONE_JUPITER_RADIUS_IN_KILOMETERS: Kilometer = 71492.0//
+export const ONE_NEPTUNE_RADIUS_IN_KILOMETERS: Kilometer = 24764.0//
+export const ONE_EARTH_RADIUS_IN_KILOMETERS: Kilometer = 6378.137//
 
 export const HUBBLE_CONSTANT = 72.0
 
@@ -97,5 +98,5 @@ export const ABSOLUTE_ZERO_TEMPERATURE_CELSIUS = -273.15
 //   [6, 'goldenHourEnd', 'goldenHour']
 // ]
 
-export const SUN_EVENTS_ALTITUDES = [-0.833, -6, -12, -18]
-export const SUN_EXTENDED_EVENTS_ALTITUDES = [6, -0.3, -0.833, -6, -12, -18]
+export const SUN_EVENTS_ALTITUDES: Degree[] = [-0.833, -6, -12, -18]
+export const SUN_EXTENDED_EVENTS_ALTITUDES: Degree[] = [6, -0.3, -0.833, -6, -12, -18]
