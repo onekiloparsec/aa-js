b5e5b1e864e77bf7bb46a6e85bca0c53e9ff7012
Cédric Foellmi <cedric@onekiloparsec.dev>
2022-05-27 09:05:56 +0200

    Back to functions
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- src/jupiter/jupiterDetails.ts
+++ src/jupiter/jupiterDetails.ts
@@ -3,6 +3,7 @@ import { DEG2RAD, RAD2DEG } from '../constants'
 import { MapTo0To360Range } from '../utils'
 import { getMeanObliquityOfEcliptic } from '../nutation'
 import { Earth } from '../Earth'
+import { getEclipticLatitude, getEclipticLongitude, getRadiusVector } from './coordinates'
 
 const sin = Math.sin
 const cos = Math.cos
@@ -11,157 +12,155 @@ const atan2 = Math.atan2
 const sqrt = Math.sqrt
 const tan = Math.tan
 
-/// <reference path="coordinates.ts" />
-export namespace Jupiter {
-  function computeJupiterDetails (jd: JulianDay) {
-    //Step 1
-    // const d = jd - 2433282.5
-    // const T1 = d / 36525
-    // const alpha0 = 268.00 + 0.1061 * T1
-    // const alpha0rad = DEG2RAD * alpha0
-    // const delta0 = 64.50 - 0.0164 * T1
-    // const delta0rad = DEG2RAD * delta0
+function computeJupiterDetails (jd: JulianDay) {
+  //Step 1
+  // const d = jd - 2433282.5
+  // const T1 = d / 36525
+  // const alpha0 = 268.00 + 0.1061 * T1
+  // const alpha0rad = DEG2RAD * alpha0
+  // const delta0 = 64.50 - 0.0164 * T1
+  // const delta0rad = DEG2RAD * delta0
 
-    //Step 3
-    const l0 = Earth.getEclipticLongitude(jd)
-    const l0rad = DEG2RAD * l0
-    const b0 = Earth.getEclipticLatitude(jd)
-    const b0rad = DEG2RAD * b0
-    const R = Earth.getRadiusVector(jd)
+  //Step 3
+  const l0 = Earth.getEclipticLongitude(jd)
+  const l0rad = DEG2RAD * l0
+  const b0 = Earth.getEclipticLatitude(jd)
+  const b0rad = DEG2RAD * b0
+  const R = Earth.getRadiusVector(jd)
 
-    //Step 4
-    let l = getEclipticLongitude(jd)
-    let lrad = DEG2RAD * l
-    const b = getEclipticLatitude(jd)
-    const brad = DEG2RAD * b
-    const r = getRadiusVector(jd)
+  //Step 4
+  let l = getEclipticLongitude(jd)
+  let lrad = DEG2RAD * l
+  const b = getEclipticLatitude(jd)
+  const brad = DEG2RAD * b
+  const r = getRadiusVector(jd)
 
-    //Step 5
-    let x = r * cos(brad) * cos(lrad) - R * cos(l0rad)
-    let y = r * cos(brad) * sin(lrad) - R * sin(l0rad)
-    let z = r * sin(brad) - R * sin(b0rad)
-    let DELTA = sqrt(x * x + y * y + z * z)
+  //Step 5
+  let x = r * cos(brad) * cos(lrad) - R * cos(l0rad)
+  let y = r * cos(brad) * sin(lrad) - R * sin(l0rad)
+  let z = r * sin(brad) - R * sin(b0rad)
+  let DELTA = sqrt(x * x + y * y + z * z)
 
-    //Step 6
-    l -= 0.012990 * DELTA / (r * r)
-    lrad = DEG2RAD * l
+  //Step 6
+  l -= 0.012990 * DELTA / (r * r)
+  lrad = DEG2RAD * l
 
-    //Step 7
-    x = r * cos(brad) * cos(lrad) - R * cos(l0rad)
-    y = r * cos(brad) * sin(lrad) - R * sin(l0rad)
-    z = r * sin(brad) - R * sin(b0rad)
-    DELTA = sqrt(x * x + y * y + z * z)
+  //Step 7
+  x = r * cos(brad) * cos(lrad) - R * cos(l0rad)
+  y = r * cos(brad) * sin(lrad) - R * sin(l0rad)
+  z = r * sin(brad) - R * sin(b0rad)
+  DELTA = sqrt(x * x + y * y + z * z)
 
-    //Step 8
-    let e0 = getMeanObliquityOfEcliptic(jd)
-    let e0rad = DEG2RAD * e0
+  //Step 8
+  let e0 = getMeanObliquityOfEcliptic(jd)
+  let e0rad = DEG2RAD * e0
 
-    //Step 11
-    const u = y * cos(e0rad) - z * sin(e0rad)
-    const v = y * sin(e0rad) + z * cos(e0rad)
-    let alpharad = atan2(u, x)
-    let alpha = RAD2DEG * alpharad
-    const deltarad = atan2(v, sqrt(x * x + u * u))
-    let delta = RAD2DEG * deltarad
+  //Step 11
+  const u = y * cos(e0rad) - z * sin(e0rad)
+  const v = y * sin(e0rad) + z * cos(e0rad)
+  let alpharad = atan2(u, x)
+  let alpha = RAD2DEG * alpharad
+  const deltarad = atan2(v, sqrt(x * x + u * u))
+  let delta = RAD2DEG * deltarad
 
-    return { alpha, delta, r, DELTA }
+  return { alpha, delta, r, DELTA }
+}
+
+export function getPlanetocentricDeclinationOfTheSun (jd: JulianDay): Degree {
+  const d = jd - 2433282.5
+  const T1 = d / 36525
+  const alpha0 = 268.00 + 0.1061 * T1
+  const alpha0rad = DEG2RAD * alpha0
+  const delta0 = 64.50 - 0.0164 * T1
+  const delta0rad = DEG2RAD * delta0
+
+  const { r, DELTA } = computeJupiterDetails(jd)
+
+  let l = getEclipticLongitude(jd)
+  l -= 0.012990 * DELTA / (r * r)
+  const lrad = DEG2RAD * l
+
+  const b = getEclipticLatitude(jd)
+  const brad = DEG2RAD * b
+
+  //Step 8
+  let e0 = getMeanObliquityOfEcliptic(jd)
+  let e0rad = DEG2RAD * e0
+
+  //Step 9
+  const alphas = atan2(cos(e0rad) * sin(lrad) - sin(e0rad) * tan(brad), cos(lrad))
+  const deltas = asin(cos(e0rad) * sin(brad) + sin(e0rad) * cos(brad) * sin(lrad))
+
+  //Step 10 DS
+  return RAD2DEG * (asin(-sin(delta0rad) * sin(deltas) - cos(delta0rad) * cos(deltas) * cos(alpha0rad - alphas)))
+}
+
+export function getPlanetocentricDeclinationOfTheEarth (jd: JulianDay): Degree {
+  const d = jd - 2433282.5
+  const T1 = d / 36525
+  const alpha0 = 268.00 + 0.1061 * T1
+  const alpha0rad = DEG2RAD * alpha0
+  const delta0 = 64.50 - 0.0164 * T1
+  const delta0rad = DEG2RAD * delta0
+
+  const { alpha, delta } = computeJupiterDetails(jd)
+  const alpharad = alpha * DEG2RAD
+  const deltarad = delta * DEG2RAD
+
+  //Step 12 DE
+  return RAD2DEG * (asin(-sin(delta0rad) * sin(deltarad) - cos(delta0rad) * cos(deltarad) * cos(alpha0rad - alpharad)))
+}
+
+export function getCentralMeridianLongitudes (jd: JulianDay): Object {
+  const d = jd - 2433282.5
+  const T1 = d / 36525
+  const alpha0 = 268.00 + 0.1061 * T1
+  const alpha0rad = DEG2RAD * alpha0
+  const delta0 = 64.50 - 0.0164 * T1
+  const delta0rad = DEG2RAD * delta0
+
+  //Step 3
+  const l0 = Earth.getEclipticLongitude(jd)
+  const l0rad = DEG2RAD * l0
+  // const b0 = Earth.getgetEclipticLatitude(jd)
+  // const b0rad = DEG2RAD * b0
+  const R = Earth.getRadiusVector(jd)
+
+  const { alpha, delta, r, DELTA } = computeJupiterDetails(jd)
+  const alpharad = alpha * DEG2RAD
+  const deltarad = delta * DEG2RAD
+
+  let l = getEclipticLongitude(jd)
+  l -= 0.012990 * DELTA / (r * r)
+  const lrad = DEG2RAD * l
+
+  //Step 2
+  const W1 = MapTo0To360Range(17.710 + 877.90003539 * d)
+  const W2 = MapTo0To360Range(16.838 + 870.27003539 * d)
+
+  const xi = atan2(sin(delta0rad) * cos(deltarad) * cos(alpha0rad - alpharad) - sin(deltarad) * cos(delta0rad), cos(deltarad) * sin(alpha0rad - alpharad))
+
+  //Step 13
+  const Geometricw1 = MapTo0To360Range(W1 - RAD2DEG * xi - 5.07033 * DELTA)
+  const Geometricw2 = MapTo0To360Range(W2 - RAD2DEG * xi - 5.02626 * DELTA)
+
+  //Step 14
+  const C = 57.2958 * (2 * r * DELTA + R * R - r * r - DELTA * DELTA) / (4 * r * DELTA)
+  let Apparentw1
+  let Apparentw2
+  if (sin(lrad - l0rad) > 0) {
+    Apparentw1 = MapTo0To360Range(Geometricw1 + C)
+    Apparentw2 = MapTo0To360Range(Geometricw2 + C)
+  } else {
+    Apparentw1 = MapTo0To360Range(Geometricw1 - C)
+    Apparentw2 = MapTo0To360Range(Geometricw2 - C)
   }
 
-  export function getPlanetocentricDeclinationOfTheSun (jd: JulianDay): Degree {
-    const d = jd - 2433282.5
-    const T1 = d / 36525
-    const alpha0 = 268.00 + 0.1061 * T1
-    const alpha0rad = DEG2RAD * alpha0
-    const delta0 = 64.50 - 0.0164 * T1
-    const delta0rad = DEG2RAD * delta0
-
-    const { r, DELTA } = computeJupiterDetails(jd)
-
-    let l = getEclipticLongitude(jd)
-    l -= 0.012990 * DELTA / (r * r)
-    const lrad = DEG2RAD * l
-
-    const b = getEclipticLatitude(jd)
-    const brad = DEG2RAD * b
-
-    //Step 8
-    let e0 = getMeanObliquityOfEcliptic(jd)
-    let e0rad = DEG2RAD * e0
-
-    //Step 9
-    const alphas = atan2(cos(e0rad) * sin(lrad) - sin(e0rad) * tan(brad), cos(lrad))
-    const deltas = asin(cos(e0rad) * sin(brad) + sin(e0rad) * cos(brad) * sin(lrad))
-
-    //Step 10 DS
-    return RAD2DEG * (asin(-sin(delta0rad) * sin(deltas) - cos(delta0rad) * cos(deltas) * cos(alpha0rad - alphas)))
-  }
-
-  export function getPlanetocentricDeclinationOfTheEarth (jd: JulianDay): Degree {
-    const d = jd - 2433282.5
-    const T1 = d / 36525
-    const alpha0 = 268.00 + 0.1061 * T1
-    const alpha0rad = DEG2RAD * alpha0
-    const delta0 = 64.50 - 0.0164 * T1
-    const delta0rad = DEG2RAD * delta0
-
-    const { alpha, delta } = computeJupiterDetails(jd)
-    const alpharad = alpha * DEG2RAD
-    const deltarad = delta * DEG2RAD
-
-    //Step 12 DE
-    return RAD2DEG * (asin(-sin(delta0rad) * sin(deltarad) - cos(delta0rad) * cos(deltarad) * cos(alpha0rad - alpharad)))
-  }
-
-  export function getCentralMeridianLongitudes (jd: JulianDay): Object {
-    const d = jd - 2433282.5
-    const T1 = d / 36525
-    const alpha0 = 268.00 + 0.1061 * T1
-    const alpha0rad = DEG2RAD * alpha0
-    const delta0 = 64.50 - 0.0164 * T1
-    const delta0rad = DEG2RAD * delta0
-
-    //Step 3
-    const l0 = Earth.getEclipticLongitude(jd)
-    const l0rad = DEG2RAD * l0
-    // const b0 = Earth.getgetEclipticLatitude(jd)
-    // const b0rad = DEG2RAD * b0
-    const R = Earth.getRadiusVector(jd)
-
-    const { alpha, delta, r, DELTA } = computeJupiterDetails(jd)
-    const alpharad = alpha * DEG2RAD
-    const deltarad = delta * DEG2RAD
-
-    let l = getEclipticLongitude(jd)
-    l -= 0.012990 * DELTA / (r * r)
-    const lrad = DEG2RAD * l
-
-    //Step 2
-    const W1 = MapTo0To360Range(17.710 + 877.90003539 * d)
-    const W2 = MapTo0To360Range(16.838 + 870.27003539 * d)
-
-    const xi = atan2(sin(delta0rad) * cos(deltarad) * cos(alpha0rad - alpharad) - sin(deltarad) * cos(delta0rad), cos(deltarad) * sin(alpha0rad - alpharad))
-
-    //Step 13
-    const Geometricw1 = MapTo0To360Range(W1 - RAD2DEG * xi - 5.07033 * DELTA)
-    const Geometricw2 = MapTo0To360Range(W2 - RAD2DEG * xi - 5.02626 * DELTA)
-
-    //Step 14
-    const C = 57.2958 * (2 * r * DELTA + R * R - r * r - DELTA * DELTA) / (4 * r * DELTA)
-    let Apparentw1
-    let Apparentw2
-    if (sin(lrad - l0rad) > 0) {
-      Apparentw1 = MapTo0To360Range(Geometricw1 + C)
-      Apparentw2 = MapTo0To360Range(Geometricw2 + C)
-    } else {
-      Apparentw1 = MapTo0To360Range(Geometricw1 - C)
-      Apparentw2 = MapTo0To360Range(Geometricw2 - C)
-    }
-
-    return {
-      geometricSystemI: Geometricw1,
-      geometricSystemII: Geometricw2,
-      apparentSystemI: Apparentw1,
-      apparentSystemII: Apparentw2
-    }
+  return {
+    geometricSystemI: Geometricw1,
+    geometricSystemII: Geometricw2,
+    apparentSystemI: Apparentw1,
+    apparentSystemII: Apparentw2
   }
 }
+
--- src/jupiter/planetaryBase.ts
+++ src/jupiter/planetaryBase.ts
@@ -7,42 +7,39 @@ function getK (jd: JulianDay): number {
   return Math.floor(0.08430 * (decimalYear - 2011.20))
 }
 
-/// <reference path="coordinates.ts" />
-export namespace Jupiter {
-  /**
-   * Aphelion (time of passage at the closest point to the Sun)
-   * @param {JulianDay} jd The julian day
-   * @returns {JulianDay}
-   */
-  export function getAphelion (jd: JulianDay): JulianDay {
-    const kdash = getK(jd) + 0.5
-    return 2455636.936 + 4332.897065 * kdash + 0.0001367 * kdash * kdash
-  }
+/**
+ * Aphelion (time of passage at the closest point to the Sun)
+ * @param {JulianDay} jd The julian day
+ * @returns {JulianDay}
+ */
+export function getAphelion (jd: JulianDay): JulianDay {
+  const kdash = getK(jd) + 0.5
+  return 2455636.936 + 4332.897065 * kdash + 0.0001367 * kdash * kdash
+}
 
-  /**
-   * Perihelion (time of passage at the farthest point to the Sun)
-   * @param {JulianDay} jd The julian day
-   * @returns {JulianDay}
-   */
-  export function getPerihelion (jd: JulianDay): JulianDay {
-    const k = getK(jd)
-    return 2455636.936 + 4332.897065 * k + 0.0001367 * k * k
-  }
+/**
+ * Perihelion (time of passage at the farthest point to the Sun)
+ * @param {JulianDay} jd The julian day
+ * @returns {JulianDay}
+ */
+export function getPerihelion (jd: JulianDay): JulianDay {
+  const k = getK(jd)
+  return 2455636.936 + 4332.897065 * k + 0.0001367 * k * k
+}
 
-  /**
-   * Planet constants, copied from the JPL
-   * Reference: https://ssd.jpl.nasa.gov/?planet_phys_par
-   */
-  export const constants: PlanetaryConstants = {
-    equatorialRadius: 71492.0,
-    meanRadius: 69911.0,
-    mass: 1898.187,
-    bulkDensity: 1.3262,
-    siderealRotationPeriod: 0.41354,
-    siderealOrbitPeriod: 11.862615,
-    visualMagnitude: -9.40,
-    geometricAlbedo: 0.52,
-    equatorialGravity: 24.79,
-    escapeVelocity: 60.20
-  }
+/**
+ * Planet constants, copied from the JPL
+ * Reference: https://ssd.jpl.nasa.gov/?planet_phys_par
+ */
+export const constants: PlanetaryConstants = {
+  equatorialRadius: 71492.0,
+  meanRadius: 69911.0,
+  mass: 1898.187,
+  bulkDensity: 1.3262,
+  siderealRotationPeriod: 0.41354,
+  siderealOrbitPeriod: 11.862615,
+  visualMagnitude: -9.40,
+  geometricAlbedo: 0.52,
+  equatorialGravity: 24.79,
+  escapeVelocity: 60.20
 }
