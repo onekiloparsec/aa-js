4c8ff49f1708451002da8be95bc9d8a56e19986e
Cedric Foellmi <cedric.foellmi@aselta.com>
2022-06-02 18:26:15 +0200

    Rebaming files to avoid confusions

--- tests/juliandays.test.js
+++ tests/juliandays.test.js
@@ -1,32 +1,32 @@
-import { julianday } from '../src'
+import { juliandays } from '../src'
 
 test('build julianday with date', () => {
   const UTCDate = new Date(Date.UTC(2016, 8, 17))
-  expect(julianday.getJulianDay(UTCDate)).toBe(2457648.5)
+  expect(juliandays.getJulianDay(UTCDate)).toBe(2457648.5)
 })
 
 test('build julianday with nothing', () => {
-  expect(julianday.getJulianDay()).toBeGreaterThan(2457648.5)
+  expect(juliandays.getJulianDay()).toBeGreaterThan(2457648.5)
 })
 
 test('build julianday with one value', () => {
   const UTCDate = new Date(Date.UTC(2016, 0, 0))
-  expect(julianday.getJulianDay(2016)).toEqual(julianday.getJulianDay(UTCDate))
+  expect(juliandays.getJulianDay(2016)).toEqual(juliandays.getJulianDay(UTCDate))
 })
 
 test('build julianday with two values', () => {
   const UTCDate = new Date(Date.UTC(2016, 8, 0))
-  expect(julianday.getJulianDay(2016, 8)).toEqual(julianday.getJulianDay(UTCDate))
+  expect(juliandays.getJulianDay(2016, 8)).toEqual(juliandays.getJulianDay(UTCDate))
 })
 
 test('build julianday with three values', () => {
   const UTCDate = new Date(Date.UTC(2016, 8, 17))
-  expect(julianday.getJulianDay(2016, 8, 17)).toEqual(julianday.getJulianDay(UTCDate))
-  expect(julianday.getJulianDay(2016, 8, 17)).toBe(2457648.5)
+  expect(juliandays.getJulianDay(2016, 8, 17)).toEqual(juliandays.getJulianDay(UTCDate))
+  expect(juliandays.getJulianDay(2016, 8, 17)).toBe(2457648.5)
 })
 
 test('julianday provide correct sidereal time', () => {
   // See AA page 88, Example 12.1a. APRIL = Month 3, not 4!
   const UTCDate = new Date(Date.UTC(1987, 3, 10))
-  expect(julianday.getLocalSiderealTime(julianday.getJulianDay(UTCDate), 0)).toBeCloseTo(13.1795463333, 6)
+  expect(juliandays.getLocalSiderealTime(juliandays.getJulianDay(UTCDate), 0)).toBeCloseTo(13.1795463333, 6)
 })
