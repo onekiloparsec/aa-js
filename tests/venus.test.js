00402a5ce7391e241f59584dbfb177263d4160d9
Cédric Foellmi <cedric@onekiloparsec.dev>
2023-10-14 14:19:31 +0200

    Adding test
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- tests/venus.test.js
+++ tests/venus.test.js
@@ -1,4 +1,4 @@
-import { Venus, juliandays } from '../src'
+import { juliandays, Venus, constants } from '../src'
 
 test('check that polar and equatorial semi diameters are identical', () => {
   const jd = juliandays.getJulianDay()
@@ -15,5 +15,14 @@ test('check heliocentric coordinates of Venus', () => {
   expect(Venus.getEclipticLatitude(jd)).toBeCloseTo(-2.62070, 4)
   expect(Venus.getRadiusVector(jd)).toBeCloseTo(0.724603, 4)
 })
+
+
+// See AA p.103, Example 15.a
+test('check equatorial coordinates of Venus', () => {
+  const jd = juliandays.getJulianDay(new Date(Date.UTC(1988, 2, 20, 0, 0, 0)))
+  expect(jd).toEqual(2447240.5)
+  const coords = Venus.getApparentEquatorialCoordinates(jd)
+  expect(coords.rightAscension * constants.H2DEG).toBeCloseTo(41.73129, 4)
+  expect(coords.declination).toBeCloseTo(18.44092, 4)
 })
 
