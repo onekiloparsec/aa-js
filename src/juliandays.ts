9f9d7446b6938fa257ef98aab0fbfc13602589b1
Cédric Foellmi <cedric@onekiloparsec.dev>
2023-10-19 22:53:25 +0200

    Consistency
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- src/juliandays.ts
+++ src/juliandays.ts
@@ -57,10 +57,10 @@ export function getJulianDay (...args: any[]): JulianDay | undefined {
  */
 export function getLocalSiderealTime (jd: JulianDay, lng: Degree): Hour {
   // Equ 12.1
-  const t = (jd - 2451545) / 36525
+  const T = getJulianCentury(jd)
 
   // Greenwich SiderealTime in degrees! Equ. 12.4 of AA, p. 88
-  let gmst = 280.46061837 + 360.98564736629 * (jd - 2451545) + 0.000387933 * t * t - t * t * t / 38710000
+  let gmst: Degree = 280.46061837 + 360.98564736629 * (jd - 2451545) + 0.000387933 * T * T - T * T * T / 38710000
 
   while (gmst < 0) {
     gmst = gmst + 360
