6450fa696e626a9c8c2a2e2bfaa1396be6fbe116
Cédric Foellmi <cedric@onekiloparsec.dev>
2023-10-19 22:54:24 +0200

    Fixed imports
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- tests/coordinates.test.js
+++ tests/coordinates.test.js
@@ -1,48 +1,60 @@
-import { coordinates, juliandays, sexagesimal } from '../src'
+import { getJulianDay } from '@/juliandays'
+import { H2DEG } from '@/constants'
+import { getFlatteningCorrections } from '@/earth/coordinates'
+import { getDecimal } from '@/sexagesimal'
+import {
+  getDeclinationFromEcliptic,
+  getEclipticLatitudeFromEquatorial,
+  getEclipticLongitudeFromEquatorial,
+  getParallacticAngle,
+  getRightAscensionFromEcliptic,
+  transformEquatorialToTopocentric
+} from '@/coordinates'
+import { Mars } from '@'
 
 test('parallactic angle before meridian', () => {
   const utcDate = new Date(Date.UTC(2017, 5, 14, 2, 0, 0.0))
-  let jd = juliandays.getJulianDay(utcDate)
+  let jd = getJulianDay(utcDate)
 
   // gro_j1655_40, see below
-  let ra = sexagesimal.getDecimal(16, 54, 0.14) // in hours
-  let dec = sexagesimal.getDecimal(39, 50, 44.9, false)
+  let ra = getDecimal(16, 54, 0.14) // in hours
+  let dec = getDecimal(39, 50, 44.9, false)
 
   // http://www.ls.eso.org/lasilla/Telescopes/2p2/D1p5M/
-  let lng = sexagesimal.getDecimal(70, 44, 7.662, false) // east-positive
-  let lat = sexagesimal.getDecimal(29, 15, 14.235, false)
+  let lng = getDecimal(70, 44, 7.662, false) // east-positive
+  let lat = getDecimal(29, 15, 14.235, false)
 
   // See https://www.eso.org/sci/observing/tools/calendar/ParAng.html to check values.
   let refAngle = -78.1
 
-  expect(coordinates.getParallacticAngle(jd, ra, dec, lng, lat)).toBeCloseTo(refAngle, 0)
+  expect(getParallacticAngle(jd, ra, dec, lng, lat)).toBeCloseTo(refAngle, 0)
 })
 
 test('parallactic angle after meridian', () => {
   const utcDate = new Date(Date.UTC(2017, 5, 14, 6, 0, 0.0))
-  let jd = juliandays.getJulianDay(utcDate)
+  let jd = getJulianDay(utcDate)
 
   // gro_j1655_40, see below
-  let ra = sexagesimal.getDecimal(16, 54, 0.14) // in hours
-  let dec = sexagesimal.getDecimal(39, 50, 44.9, false)
+  let ra = getDecimal(16, 54, 0.14) // in hours
+  let dec = getDecimal(39, 50, 44.9, false)
 
   // http://www.ls.eso.org/lasilla/Telescopes/2p2/D1p5M/
-  let lng = sexagesimal.getDecimal(70, 44, 7.662, false) // east-positive
-  let lat = sexagesimal.getDecimal(29, 15, 14.235, false)
+  let lng = getDecimal(70, 44, 7.662, false) // east-positive
+  let lat = getDecimal(29, 15, 14.235, false)
 
   // Slightly adjusted values. Ref might not be 100% accurate...
   // Looking for improved reference values...
   let refAngle = 74.4
 
-  expect(coordinates.getParallacticAngle(jd, ra, dec, lng, lat)).toBeCloseTo(refAngle, 0)
+  expect(getParallacticAngle(jd, ra, dec, lng, lat)).toBeCloseTo(refAngle, 0)
 })
 
 // See AA p95, Ex 13.a
 test('transform equatorial to ecliptic', () => {
   const pollux = { rightAscension: 7 + 45 / 60 + 18.946 / 3600, declination: 28 + 1 / 60 + 34.26 / 3600 }
-  const lambda = coordinates.getEclipticLongitudeFromEquatorial(pollux.rightAscension, pollux.declination)
+  const lambda = getEclipticLongitudeFromEquatorial(pollux.rightAscension, pollux.declination)
   expect(lambda).toBeCloseTo(113.215630, 6)
-  const beta = coordinates.getEclipticLatitudeFromEquatorial(pollux.rightAscension, pollux.declination)
+  const beta = getEclipticLatitudeFromEquatorial(pollux.rightAscension, pollux.declination)
   expect(beta).toBeCloseTo(6.684170, 6)
 })
 
@@ -50,8 +62,8 @@ test('transform equatorial to ecliptic', () => {
 // See AA p95, Ex 13.a
 test('transform ecliptic to equatorial', () => {
   const pollux = { rightAscension: 7 + 45 / 60 + 18.946 / 3600, declination: 28 + 1 / 60 + 34.26 / 3600 }
-  const alpha = coordinates.getRightAscensionFromEcliptic(113.215630, 6.684170)
+  const alpha = getRightAscensionFromEcliptic(113.215630, 6.684170)
   expect(alpha).toBeCloseTo(pollux.rightAscension, 6)
-  const delta = coordinates.getDeclinationFromEcliptic(113.215630, 6.684170)
+  const delta = getDeclinationFromEcliptic(113.215630, 6.684170)
   expect(delta).toBeCloseTo(pollux.declination, 6)
 })
