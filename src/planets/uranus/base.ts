9672af7f7ab1b3e71a4217eaba1d2a46841113f9
Cédric Foellmi <cedric@onekiloparsec.dev>
2023-10-20 22:22:52 +0200

    Upgrading Uranus
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- src/planets/uranus/base.ts
+++ src/planets/uranus/base.ts
@@ -26,20 +26,3 @@ export function getPerihelion (jd: JulianDay): JulianDay {
   const k = getK(jd)
   return 2470213.5 + 30694.8767 * k - 0.00541 * k * k
 }
-
-/**
- * Planet constants, copied from the JPL
- * Reference: https://ssd.jpl.nasa.gov/?planet_phys_par
- */
-export const constants: PlanetConstants = {
-  equatorialRadius: 25559,
-  meanRadius: 25362,
-  mass: 86.8127,
-  bulkDensity: 1.270,
-  siderealRotationPeriod: -0.71833, // negative
-  siderealOrbitPeriod: 84.016846,
-  visualMagnitude: -7.19,
-  geometricAlbedo: 0.51,
-  equatorialGravity: 8.87,
-  escapeVelocity: 21.38
-}
--- src/planets/uranus/index.ts
+++ src/planets/uranus/index.ts
@@ -1,36 +1,69 @@
+import { Planet } from '@/types'
 import {
-  getApparentEquatorialCoordinates,
   getEclipticCoordinates,
   getEclipticLatitude,
   getEclipticLongitude,
   getEquatorialCoordinates,
   getRadiusVector
 } from './coordinates'
-import { constants, getAphelion, getPerihelion } from './planetaryBase'
+import { constants, orbitalElements, orbitalElementsJ2000 } from './constants'
+import { getAphelion, getPerihelion } from './base'
+import {
+  getGeocentricDistance,
+  getGeocentricEclipticCoordinates,
+  getGeocentricEquatorialCoordinates,
+  getInstantaneousVelocity,
+  getLengthOfEllipse,
+  getVelocityAtAphelion,
+  getVelocityAtPerihelion
+} from './elliptical'
 import {
   getEquatorialSemiDiameter,
   getIlluminatedFraction,
   getMagnitude,
   getPhaseAngle,
-  getPlanetaryDetails,
   getPolarSemiDiameter
-} from './planetaryDetails'
-import { Planet } from '@/types'
+} from './details'
+import {
+  getEccentricity,
+  getInclination,
+  getLongitudeOfAscendingNode,
+  getLongitudeOfPerihelion,
+  getMeanLongitude
+} from './orbital'
 
 export const Uranus: Planet = {
+  // Heliocentric coordinates
   getEclipticLongitude,
   getEclipticLatitude,
   getEclipticCoordinates,
   getEquatorialCoordinates,
-  getApparentEquatorialCoordinates,
+  getRadiusVector,
+  // Geocentric coordinates
+  getGeocentricDistance,
+  getGeocentricEclipticCoordinates,
+  getGeocentricEquatorialCoordinates,
+  // Planet elliptical properties
+  getInstantaneousVelocity,
+  getVelocityAtPerihelion,
+  getVelocityAtAphelion,
+  getLengthOfEllipse,
+  // Planet orbital properties
+  getMeanLongitude,
+  getEccentricity,
+  getInclination,
+  getLongitudeOfAscendingNode,
+  getLongitudeOfPerihelion,
+  // Planet base properties
   getAphelion,
   getPerihelion,
-  constants,
-  getRadiusVector,
-  getPlanetaryDetails,
   getPhaseAngle,
   getIlluminatedFraction,
   getMagnitude,
   getEquatorialSemiDiameter,
-  getPolarSemiDiameter
+  getPolarSemiDiameter,
+  // Fixed values
+  constants,
+  orbitalElements,
+  orbitalElementsJ2000,
 }
