3a8f3e06c29672ed744919e05bc2ccac696fc5de
Cédric Foellmi <cedric@onekiloparsec.dev>
2023-10-21 23:37:16 +0200

    Adopting Decimals for Sun, and exporting a dedicated object for consistency
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- src/sun.ts
+++ src/sun.ts
@@ -1,29 +1,30 @@
 /**
  @module Sun
-*/
-import { Degree, EclipticCoordinates, EquatorialCoordinates, JulianCentury, JulianDay } from './types'
-import { DEG2RAD } from './constants'
-import { transformEclipticToEquatorial } from './coordinates'
-import { getCorrectionInLatitude, getCorrectionInLongitude } from './fk5'
-import { getMeanObliquityOfEcliptic, getNutationInLongitude, getTrueObliquityOfEcliptic } from './nutation'
-import { getDecimal } from './sexagesimal'
-import { fmod360 } from './utils'
-import { Earth } from './earth'
-
-const sin = Math.sin
+ */
+import Decimal from 'decimal.js'
+import { Degree, EclipticCoordinates, EquatorialCoordinates, JulianCentury, JulianDay } from '@/types'
+import { DEG2RAD } from '@/constants'
+import { getJulianCentury } from '@/juliandays'
+import { transformEclipticToEquatorial } from '@/coordinates'
+import { getCorrectionInLatitude, getCorrectionInLongitude } from '@/fk5'
+import { getMeanObliquityOfEcliptic, getNutationInLongitude, getTrueObliquityOfEcliptic } from '@/nutation'
+import { getDecimalValue } from '@/sexagesimal'
+import { fmod360 } from '@/utils'
+import { Earth } from '@/earth'
 
 /**
  * Computes the Sun mean anomaly which is equal to the mean anomaly of the Earth.
  * @param  {JulianDay} jd The julian day
  * @returns {Degree} The sun mean anomaly
  */
-export function getMeanAnomaly (jd: JulianDay): Degree {
-  const T = (jd - 2451545) / 36525
-  const T2 = T * T
-  const T3 = T2 * T
+function getMeanAnomaly (jd: JulianDay | number): Degree {
+  const T = getJulianCentury(jd)
   // In AA++ (C++) implementation, values differ a little bit. But we prefer textbook AA values to ensure tests validity
   // In AA++ : fmod360(357.5291092 + 35999.0502909 * T - 0.0001536 * T2 + T3 / 24490000)
-  return fmod360(357.52911 + 35999.05029 * T - 0.0001537 * T2 + T3 / 24490000)
+  return fmod360(new Decimal(357.52911)
+    .plus(new Decimal(35999.05029).mul(T))
+    .minus(new Decimal(0.0001537).mul(T.pow(2)))
+    .plus(T.pow(3).dividedBy(24490000)))
 }
 
 /**
@@ -31,11 +32,11 @@ export function getMeanAnomaly (jd: JulianDay): Degree {
  * @param  {JulianDay} jd The julian day
  * @returns {Degree} The Sun true anomaly
  */
-export function getTrueAnomaly (jd: JulianDay): Degree {
-  const T = (jd - 2451545) / 36525
+function getTrueAnomaly (jd: JulianDay | number): Degree {
+  const T = getJulianCentury(jd)
   const M = getMeanAnomaly(jd)
   const C = getEquationOfTheCenter(T, M)
-  return fmod360(M + C)
+  return fmod360(M.plus(C))
 }
 
 /**
@@ -45,10 +46,17 @@ export function getTrueAnomaly (jd: JulianDay): Degree {
  * @param {Degree} M
  * @return {Degree}
  */
-export function getEquationOfTheCenter (T: JulianCentury, M: Degree): Degree {
-  return (1.914602 - 0.004817 * T - 0.000014 * T * T) * sin(M * DEG2RAD) +
-    (0.019993 - 0.000101 * T) * sin(2 * M * DEG2RAD) +
-    0.000289 * sin(3 * M * DEG2RAD)
+function getEquationOfTheCenter (T: JulianCentury | number, M: Degree | number): Degree {
+  return (
+    new Decimal(1.914602)
+      .minus(new Decimal(0.004817).mul(T))
+      .minus(new Decimal(0.000014).mul(T).mul(T))
+  )
+    .mul(Decimal.sin(new Decimal(M).mul(DEG2RAD)))
+    .plus((new Decimal(0.019993)
+      .minus(new Decimal(0.000101).mul(T)))
+      .mul(Decimal.sin(new Decimal(2).mul(M).mul(DEG2RAD))))
+    .plus(new Decimal(0.000289).mul(Decimal.sin(new Decimal(3).mul(M).mul(DEG2RAD))))
 }
 
 /**
@@ -57,8 +65,12 @@ export function getEquationOfTheCenter (T: JulianCentury, M: Degree): Degree {
  * @param {JulianCentury} T The julian century
  * @return {Degree}
  */
-export function getMeanLongitudeReferredToMeanEquinoxOfDate (T: JulianCentury): Degree {
-  return fmod360(280.46646 + 36000.76983 * T + 0.0003032 * T * T)
+function getMeanLongitudeReferredToMeanEquinoxOfDate (T: JulianCentury | number): Degree {
+  return fmod360(
+    new Decimal(280.46646)
+      .plus(new Decimal(36000.76983).mul(T))
+      .plus(new Decimal(0.0003032).mul(new Decimal(T).pow(2)))
+  )
 }
 
 /**
@@ -70,12 +82,12 @@ export function getMeanLongitudeReferredToMeanEquinoxOfDate (T: JulianCentury): 
  * @param {JulianDay} jd The julian day
  * @returns {Degree}
  */
-export function getGeometricEclipticLongitude (jd: JulianDay): Degree {
-  const T = (jd - 2451545.0) / 36525.0
+function getGeometricEclipticLongitude (jd: JulianDay | number): Degree {
+  const T = getJulianCentury(jd)
   const L0 = getMeanLongitudeReferredToMeanEquinoxOfDate(T)
   const M = getMeanAnomaly(jd)
   const C = getEquationOfTheCenter(T, M)
-  return fmod360(L0 + C)
+  return fmod360(L0.plus(C))
 }
 
 // --- high accuracy geocentric longitude and latitude of the Sun. See AA pp 166.
@@ -85,8 +97,8 @@ export function getGeometricEclipticLongitude (jd: JulianDay): Degree {
  * @param {JulianDay} jd The julian day
  * @returns {Degree}
  */
-export function getGeocentricEclipticLongitude (jd: JulianDay): Degree {
-  return fmod360(Earth.getEclipticLongitude(jd) + 180)
+function getGeocentricEclipticLongitude (jd: JulianDay | number): Degree {
+  return fmod360(Earth.getEclipticLongitude(jd).plus(180))
 }
 
 /**
@@ -94,8 +106,8 @@ export function getGeocentricEclipticLongitude (jd: JulianDay): Degree {
  * @param {JulianDay} jd The julian day
  * @returns {Degree}
  */
-export function getGeocentricEclipticLatitude (jd: JulianDay): Degree {
-  return -Earth.getEclipticLatitude(jd)
+function getGeocentricEclipticLatitude (jd: JulianDay | number): Degree {
+  return new Decimal(-1).mul(Earth.getEclipticLatitude(jd))
 }
 
 /**
@@ -103,8 +115,8 @@ export function getGeocentricEclipticLatitude (jd: JulianDay): Degree {
  * @param {JulianDay} jd The julian day
  * @returns {Degree}
  */
-export function getGeocentricEclipticLongitudeJ2000 (jd: JulianDay): Degree {
-  return fmod360(Earth.getEclipticLongitudeJ2000(jd) + 180)
+function getGeocentricEclipticLongitudeJ2000 (jd: JulianDay | number): Degree {
+  return fmod360(Earth.getEclipticLongitudeJ2000(jd).plus(180))
 }
 
 /**
@@ -112,27 +124,27 @@ export function getGeocentricEclipticLongitudeJ2000 (jd: JulianDay): Degree {
  * @param {JulianDay} jd The julian day
  * @returns {Degree}
  */
-export function getGeocentricEclipticLatitudeJ2000 (jd: JulianDay): Degree {
-  return -Earth.getEclipticLatitudeJ2000(jd)
+function getGeocentricEclipticLatitudeJ2000 (jd: JulianDay | number): Degree {
+  return new Decimal(-1).mul(Earth.getEclipticLatitudeJ2000(jd))
 }
 
 // --- Conversion from high-accuracy geocentric coordinates of the Sun to the FK5 system. See AA p 166
 
 // See AA+ CAASun::GeometricFK5EclipticLongitude
-export function getGeometricFK5EclipticLongitude (jd: JulianDay): Degree {
+function getGeometricFK5EclipticLongitude (jd: JulianDay | number): Degree {
   // Convert to the FK5 system
   let Longitude = getGeocentricEclipticLongitude(jd)
   const Latitude = getGeocentricEclipticLatitude(jd)
-  Longitude += getCorrectionInLongitude(Longitude, Latitude, jd)
+  Longitude = Longitude.plus(getCorrectionInLongitude(Longitude, Latitude, jd))
   return Longitude
 }
 
 // See AA+ CAASun::GeometricFK5EclipticLatitude
-export function getGeometricFK5EclipticLatitude (jd: JulianDay): Degree {
+function getGeometricFK5EclipticLatitude (jd: JulianDay | number): Degree {
   // Convert to the FK5 system
   const Longitude = getGeocentricEclipticLongitude(jd)
   let Latitude = getGeocentricEclipticLatitude(jd)
-  Latitude += getCorrectionInLatitude(jd, Longitude)
+  Latitude = Latitude.plus(getCorrectionInLatitude(jd, Longitude))
   return Latitude
 }
 
@@ -144,15 +156,15 @@ export function getGeometricFK5EclipticLatitude (jd: JulianDay): Degree {
  * @param {JulianDay} jd The julian day
  * @returns {Degree}
  */
-export function getApparentEclipticLongitude (jd: JulianDay): Degree {
+function getApparentEclipticLongitude (jd: JulianDay | number): Degree {
   let Longitude = getGeometricFK5EclipticLongitude(jd)
 
   // Apply the correction in longitude due to nutation
-  Longitude += getDecimal(0, 0, getNutationInLongitude(jd))
+  Longitude = Longitude.plus(getDecimalValue(0, 0, getNutationInLongitude(jd)))
 
   // Apply the correction in longitude due to aberration
   const R = Earth.getRadiusVector(jd)
-  Longitude -= getDecimal(0, 0, 20.4898 / R)
+  Longitude = Longitude.minus(getDecimalValue(0, 0, new Decimal(20.4898).dividedBy(R)))
 
   return Longitude
 }
@@ -163,7 +175,7 @@ export function getApparentEclipticLongitude (jd: JulianDay): Degree {
  * @param {JulianDay} jd The julian day
  * @returns {Degree}
  */
-export function getApparentEclipticLatitude (jd: JulianDay): Degree {
+function getApparentEclipticLatitude (jd: JulianDay | number): Degree {
   return getGeometricFK5EclipticLatitude(jd)
 }
 
@@ -172,7 +184,7 @@ export function getApparentEclipticLatitude (jd: JulianDay): Degree {
  * @param {JulianDay} jd The julian day
  * @returns {EclipticCoordinates}
  */
-export function getApparentEclipticCoordinates (jd: JulianDay): EclipticCoordinates {
+function getApparentEclipticCoordinates (jd: JulianDay | number): EclipticCoordinates {
   return {
     longitude: getApparentEclipticLongitude(jd),
     latitude: getApparentEclipticLatitude(jd)
@@ -184,7 +196,7 @@ export function getApparentEclipticCoordinates (jd: JulianDay): EclipticCoordina
  * @param {JulianDay} jd The julian day
  * @returns {EquatorialCoordinates}
  */
-export function getApparentEquatorialCoordinates (jd: JulianDay): EquatorialCoordinates {
+function getApparentEquatorialCoordinates (jd: JulianDay | number): EquatorialCoordinates {
   return transformEclipticToEquatorial(
     getApparentEclipticLongitude(jd),
     getApparentEclipticLatitude(jd),
@@ -197,35 +209,34 @@ export function getApparentEquatorialCoordinates (jd: JulianDay): EquatorialCoor
  * @param {JulianDay} jd The julian day
  * @returns {Degree}
  */
-export function getVariationGeometricEclipticLongitude (jd: JulianDay): Degree {
+function getVariationGeometricEclipticLongitude (jd: JulianDay | number): Degree {
   // D is the number of days since the epoch
-  const D = jd - 2451545.00
-  const tau = (D / 365250)
-  const tau2 = tau * tau
-  const tau3 = tau2 * tau
+  const D = new Decimal(jd).minus(2451545.00)
+  const tau = D.dividedBy(365250)
+  const deg2rad = new Decimal(DEG2RAD)
 
-  return 3548.193 +
-    118.568 * sin(DEG2RAD * (87.5287 + 359993.7286 * tau)) +
-    2.476 * sin(DEG2RAD * (85.0561 + 719987.4571 * tau)) +
-    1.376 * sin(DEG2RAD * (27.8502 + 4452671.1152 * tau)) +
-    0.119 * sin(DEG2RAD * (73.1375 + 450368.8564 * tau)) +
-    0.114 * sin(DEG2RAD * (337.2264 + 329644.6718 * tau)) +
-    0.086 * sin(DEG2RAD * (222.5400 + 659289.3436 * tau)) +
-    0.078 * sin(DEG2RAD * (162.8136 + 9224659.7915 * tau)) +
-    0.054 * sin(DEG2RAD * (82.5823 + 1079981.1857 * tau)) +
-    0.052 * sin(DEG2RAD * (171.5189 + 225184.4282 * tau)) +
-    0.034 * sin(DEG2RAD * (30.3214 + 4092677.3866 * tau)) +
-    0.033 * sin(DEG2RAD * (119.8105 + 337181.4711 * tau)) +
-    0.023 * sin(DEG2RAD * (247.5418 + 299295.6151 * tau)) +
-    0.023 * sin(DEG2RAD * (325.1526 + 315559.5560 * tau)) +
-    0.021 * sin(DEG2RAD * (155.1241 + 675553.2846 * tau)) +
-    7.311 * tau * sin(DEG2RAD * (333.4515 + 359993.7286 * tau)) +
-    0.305 * tau * sin(DEG2RAD * (330.9814 + 719987.4571 * tau)) +
-    0.010 * tau * sin(DEG2RAD * (328.5170 + 1079981.1857 * tau)) +
-    0.309 * tau2 * sin(DEG2RAD * (241.4518 + 359993.7286 * tau)) +
-    0.021 * tau2 * sin(DEG2RAD * (205.0482 + 719987.4571 * tau)) +
-    0.004 * tau2 * sin(DEG2RAD * (297.8610 + 4452671.1152 * tau)) +
-    0.010 * tau3 * sin(DEG2RAD * (154.7066 + 359993.7286 * tau))
+  return new Decimal(3548.193)
+    .plus(new Decimal(118.568).mul(Decimal.sin(deg2rad.mul(new Decimal(87.5287).plus(new Decimal(359993.7286).mul(tau))))))
+    .plus(new Decimal(2.476).mul(Decimal.sin(deg2rad.mul(new Decimal(85.0561).plus(new Decimal(719987.4571).mul(tau))))))
+    .plus(new Decimal(1.376).mul(Decimal.sin(deg2rad.mul(new Decimal(27.8502).plus(new Decimal(4452671.1152).mul(tau))))))
+    .plus(new Decimal(0.119).mul(Decimal.sin(deg2rad.mul(new Decimal(73.1375).plus(new Decimal(450368.8564).mul(tau))))))
+    .plus(new Decimal(0.114).mul(Decimal.sin(deg2rad.mul(new Decimal(337.2264).plus(new Decimal(329644.6718).mul(tau))))))
+    .plus(new Decimal(0.086).mul(Decimal.sin(deg2rad.mul(new Decimal(222.5400).plus(new Decimal(659289.3436).mul(tau))))))
+    .plus(new Decimal(0.078).mul(Decimal.sin(deg2rad.mul(new Decimal(162.8136).plus(new Decimal(9224659.7915).mul(tau))))))
+    .plus(new Decimal(0.054).mul(Decimal.sin(deg2rad.mul(new Decimal(82.5823).plus(new Decimal(1079981.1857).mul(tau))))))
+    .plus(new Decimal(0.052).mul(Decimal.sin(deg2rad.mul(new Decimal(171.5189).plus(new Decimal(225184.4282).mul(tau))))))
+    .plus(new Decimal(0.034).mul(Decimal.sin(deg2rad.mul(new Decimal(30.3214).plus(new Decimal(4092677.3866).mul(tau))))))
+    .plus(new Decimal(0.033).mul(Decimal.sin(deg2rad.mul(new Decimal(119.8105).plus(new Decimal(337181.4711).mul(tau))))))
+    .plus(new Decimal(0.023).mul(Decimal.sin(deg2rad.mul(new Decimal(247.5418).plus(new Decimal(299295.6151).mul(tau))))))
+    .plus(new Decimal(0.023).mul(Decimal.sin(deg2rad.mul(new Decimal(325.1526).plus(new Decimal(315559.5560).mul(tau))))))
+    .plus(new Decimal(0.021).mul(Decimal.sin(deg2rad.mul(new Decimal(155.1241).plus(new Decimal(675553.2846).mul(tau))))))
+    .plus(new Decimal(7.311).mul(tau).mul(Decimal.sin(deg2rad.mul(new Decimal(333.4515).plus(new Decimal(359993.7286).mul(tau))))))
+    .plus(new Decimal(0.305).mul(tau).mul(Decimal.sin(deg2rad.mul(new Decimal(330.9814).plus(new Decimal(719987.4571).mul(tau))))))
+    .plus(new Decimal(0.010).mul(tau).mul(Decimal.sin(deg2rad.mul(new Decimal(328.5170).plus(new Decimal(1079981.1857).mul(tau))))))
+    .plus(new Decimal(0.309).mul(tau.pow(2)).mul(Decimal.sin(deg2rad.mul(new Decimal(241.4518).plus(new Decimal(359993.7286).mul(tau))))))
+    .plus(new Decimal(0.021).mul(tau.pow(2)).mul(Decimal.sin(deg2rad.mul(new Decimal(205.0482).plus(new Decimal(719987.4571).mul(tau))))))
+    .plus(new Decimal(0.004).mul(tau.pow(2)).mul(Decimal.sin(deg2rad.mul(new Decimal(297.8610).plus(new Decimal(4452671.1152).mul(tau))))))
+    .plus(new Decimal(0.010).mul(tau.pow(3)).mul(Decimal.sin(deg2rad.mul(new Decimal(154.7066).plus(new Decimal(359993.7286).mul(tau))))))
 }
 
 /**
@@ -233,7 +244,7 @@ export function getVariationGeometricEclipticLongitude (jd: JulianDay): Degree {
  * @param {JulianDay} jd The julian day
  * @returns EclipticCoordinates
  */
-export function getEclipticCoordinates (jd: JulianDay): EclipticCoordinates {
+function getEclipticCoordinates (jd: JulianDay | number): EclipticCoordinates {
   return {
     longitude: getGeocentricEclipticLongitude(jd),
     latitude: getGeocentricEclipticLatitude(jd)
@@ -245,7 +256,7 @@ export function getEclipticCoordinates (jd: JulianDay): EclipticCoordinates {
  * @param {JulianDay} jd The julian day
  * @returns EclipticCoordinates
  */
-export function getEclipticCoordinatesJ2000 (jd: JulianDay): EclipticCoordinates {
+function getEclipticCoordinatesJ2000 (jd: JulianDay | number): EclipticCoordinates {
   return {
     longitude: getGeocentricEclipticLongitudeJ2000(jd),
     latitude: getGeocentricEclipticLatitudeJ2000(jd)
@@ -257,7 +268,7 @@ export function getEclipticCoordinatesJ2000 (jd: JulianDay): EclipticCoordinates
  * @param {JulianDay} jd The julian day
  * @returns EquatorialCoordinates
  */
-export function getEquatorialCoordinates (jd: JulianDay): EquatorialCoordinates {
+function getEquatorialCoordinates (jd: JulianDay | number): EquatorialCoordinates {
   return transformEclipticToEquatorial(
     getGeocentricEclipticLongitude(jd),
     getGeocentricEclipticLatitude(jd),
@@ -270,12 +281,35 @@ export function getEquatorialCoordinates (jd: JulianDay): EquatorialCoordinates 
  * @param {JulianDay} jd The julian day
  * @returns EquatorialCoordinates
  */
-export function getEquatorialCoordinatesJ2000 (jd: JulianDay): EquatorialCoordinates {
+function getEquatorialCoordinatesJ2000 (jd: JulianDay | number): EquatorialCoordinates {
   return transformEclipticToEquatorial(
     getGeocentricEclipticLongitudeJ2000(jd),
     getGeocentricEclipticLatitudeJ2000(jd),
     getMeanObliquityOfEcliptic(jd)
   )
+}
+
+export const Sun = {
+  getMeanAnomaly,
+  getTrueAnomaly,
+  getEquationOfTheCenter,
+  getMeanLongitudeReferredToMeanEquinoxOfDate,
+  getGeometricEclipticLongitude,
+  getGeocentricEclipticLongitude,
+  getGeocentricEclipticLatitude,
+  getGeocentricEclipticLongitudeJ2000,
+  getGeocentricEclipticLatitudeJ2000,
+  getGeometricFK5EclipticLongitude,
+  getGeometricFK5EclipticLatitude,
+  getApparentEclipticLongitude,
+  getApparentEclipticLatitude,
+  getApparentEclipticCoordinates,
+  getApparentEquatorialCoordinates,
+  getVariationGeometricEclipticLongitude,
+  getEclipticCoordinates,
+  getEclipticCoordinatesJ2000,
+  getEquatorialCoordinates,
+  getEquatorialCoordinatesJ2000
 }
 
 //
