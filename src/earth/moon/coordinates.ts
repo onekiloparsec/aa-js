bc62f32f6b2a6d084852600ccc853656248065c5
Cédric Foellmi <cedric@onekiloparsec.dev>
2023-10-22 12:34:02 +0200

    Rewrite of Moon coordinates with Decimal.js
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- src/earth/moon/coordinates.ts
+++ src/earth/moon/coordinates.ts
@@ -1,10 +1,12 @@
+import Decimal from 'decimal.js'
 import { Degree, EclipticCoordinates, EquatorialCoordinates, JulianDay, Kilometer } from '@/types'
-import { fmod360 } from '@/utils'
-import { DEG2RAD, RAD2DEG } from '@/constants'
 import { transformEclipticToEquatorial } from '@/coordinates'
-import { getMeanObliquityOfEcliptic, getNutationInLongitude, getTrueObliquityOfEcliptic } from '@/nutation'
-import { getMeanAnomaly as getSunMeanAnomaly } from '@/sun'
+import { getJulianCentury } from '@/juliandays'
+import { DEG2RAD, RAD2DEG, TWO } from '@/constants'
+import { fmod360 } from '@/utils'
+import { Sun } from '@/sun'
 import { getEccentricity as getEarthEccentricity } from '@/earth/coordinates'
+import { getMeanObliquityOfEcliptic, getNutationInLongitude, getTrueObliquityOfEcliptic } from '@/earth/nutation'
 import { gMoonCoefficients1, gMoonCoefficients2, gMoonCoefficients3, gMoonCoefficients4 } from './coefficients'
 
 /**
@@ -12,12 +14,15 @@ import { gMoonCoefficients1, gMoonCoefficients2, gMoonCoefficients3, gMoonCoeffi
  * @param {JulianDay} jd The julian day
  * @returns {Degree}
  */
-export function getMeanLongitude (jd: JulianDay): Degree {
-  const T = (jd - 2451545) / 36525
-  const T2 = T * T
-  const T3 = T2 * T
-  const T4 = T3 * T
-  return fmod360(218.3164477 + 481267.88123421 * T - 0.0015786 * T2 + T3 / 538841 - T4 / 65194000)
+export function getMeanLongitude (jd: JulianDay | number): Degree {
+  const T = getJulianCentury(jd)
+  return fmod360(
+    new Decimal(218.3164477)
+      .plus(new Decimal(481267.88123421).mul(T))
+      .minus(new Decimal(0.0015786).mul(T.pow(2)))
+      .plus(T.pow(3).dividedBy(538841))
+      .minus(T.pow(4).dividedBy(65194000))
+  )
 }
 
 /**
@@ -25,12 +30,15 @@ export function getMeanLongitude (jd: JulianDay): Degree {
  * @param {JulianDay} jd The julian day
  * @returns {Degree}
  */
-export function getMeanElongation (jd: JulianDay): Degree {
-  const T = (jd - 2451545) / 36525
-  const T2 = T * T
-  const T3 = T2 * T
-  const T4 = T3 * T
-  return fmod360(297.8501921 + 445267.1114034 * T - 0.0018819 * T2 + T3 / 545868 - T4 / 113065000)
+export function getMeanElongation (jd: JulianDay | number): Degree {
+  const T = getJulianCentury(jd)
+  return fmod360(
+    new Decimal(297.8501921)
+      .plus(new Decimal(445267.1114034).mul(T))
+      .minus(new Decimal(0.0018819).mul(T.pow(2)))
+      .plus(T.pow(3).dividedBy(545868))
+      .minus(T.pow(4).dividedBy(113065000))
+  )
 }
 
 /**
@@ -38,12 +46,15 @@ export function getMeanElongation (jd: JulianDay): Degree {
  * @param {JulianDay} jd The julian day
  * @return {Degree}
  */
-export function getMeanAnomaly (jd: JulianDay): Degree {
-  const T = (jd - 2451545) / 36525
-  const T2 = T * T
-  const T3 = T2 * T
-  const T4 = T3 * T
-  return fmod360(134.9633964 + 477198.8675055 * T + 0.0087414 * T2 + T3 / 69699 - T4 / 14712000)
+export function getMeanAnomaly (jd: JulianDay | number): Degree {
+  const T = getJulianCentury(jd)
+  return fmod360(
+    new Decimal(134.9633964)
+      .plus(new Decimal(477198.8675055).mul(T))
+      .plus(new Decimal(0.0087414).mul(T.pow(2)))
+      .plus(T.pow(3).dividedBy(69699))
+      .minus(T.pow(4).dividedBy(14712000))
+  )
 }
 
 /**
@@ -51,12 +62,15 @@ export function getMeanAnomaly (jd: JulianDay): Degree {
  * @param {JulianDay} jd The julian day
  * @returns {Degree}
  */
-export function getArgumentOfLatitude (jd: JulianDay): Degree {
-  const T = (jd - 2451545) / 36525
-  const T2 = T * T
-  const T3 = T2 * T
-  const T4 = T3 * T
-  return fmod360(93.2720950 + 483202.0175233 * T - 0.0036539 * T2 - T3 / 3526000 + T4 / 863310000)
+export function getArgumentOfLatitude (jd: JulianDay | number): Degree {
+  const T = getJulianCentury(jd)
+  return fmod360(
+    new Decimal(93.2720950)
+      .plus(new Decimal(483202.0175233).mul(T))
+      .minus(new Decimal(0.0036539).mul(T.pow(2)))
+      .minus(T.pow(3).dividedBy(3526000))
+      .minus(T.pow(4).dividedBy(863310000))
+  )
 }
 
 /**
@@ -64,46 +78,40 @@ export function getArgumentOfLatitude (jd: JulianDay): Degree {
  * @param {JulianDay} jd The julian day
  * @returns {Degree}
  */
-export function getEclipticLongitude (jd: JulianDay): Degree {
-  const Ldash = getMeanLongitude(jd) * DEG2RAD
-  const D = getMeanElongation(jd) * DEG2RAD
-  const M = getSunMeanAnomaly(jd) * DEG2RAD
-  const Mdash = getMeanAnomaly(jd) * DEG2RAD
-  const F = getArgumentOfLatitude(jd) * DEG2RAD
+export function getEclipticLongitude (jd: JulianDay | number): Degree {
+  const Ldash = getMeanLongitude(jd).mul(DEG2RAD)
+  const D = getMeanElongation(jd).mul(DEG2RAD)
+  const M = Sun.getMeanAnomaly(jd).mul(DEG2RAD)
+  const Mdash = getMeanAnomaly(jd).mul(DEG2RAD)
+  const F = getArgumentOfLatitude(jd).mul(DEG2RAD)
   const E = getEarthEccentricity(jd)
-  const Esquared = E * E
-  const T = (jd - 2451545) / 36525
+  const T = getJulianCentury(jd)
 
-  const A1 = fmod360(119.75 + 131.849 * T) * DEG2RAD
-  const A2 = fmod360(53.09 + 479264.290 * T) * DEG2RAD
+  const A1 = fmod360(new Decimal(119.75).plus(new Decimal(131.849).mul(T))).mul(DEG2RAD)
+  const A2 = fmod360(new Decimal(53.09).plus(new Decimal(479264.290).mul(T))).mul(DEG2RAD)
 
-  let SigmaL = 0
-  for (let i = 0; i < gMoonCoefficients1.length; i++) {
-    let ThisSigma = gMoonCoefficients2[i].A *
-      Math.sin(gMoonCoefficients1[i].D * D +
-        gMoonCoefficients1[i].M * M +
-        gMoonCoefficients1[i].Mdash * Mdash +
-        gMoonCoefficients1[i].F * F)
-
-    if ((gMoonCoefficients1[i].M === 1) || (gMoonCoefficients1[i].M === -1)) {
-      ThisSigma *= E
-    } else if ((gMoonCoefficients1[i].M === 2) || (gMoonCoefficients1[i].M === -2)) {
-      ThisSigma *= Esquared
-    }
-
-    SigmaL += ThisSigma
-  }
+  let SigmaL = gMoonCoefficients1.reduce((sum, val, index) => {
+    const argument = val.D.mul(D)
+      .plus(val.M.mul(M))
+      .plus(val.Mdash.mul(Mdash))
+      .plus(val.F.mul(F))
+    const modulator = (val.M.abs().toNumber() === 1) ? E :
+      (val.M.abs().toNumber() === 2) ? E.pow(2)
+        : 1
+    // SigmaL values = "A" column of "2" coefficient array: gMoonCoefficients2. See AA p.338 & 339
+    return sum.plus(gMoonCoefficients2[index].A.mul(modulator).mul(Decimal.sin(argument)))
+  }, new Decimal(0))
 
   // Finally the additive terms
-  SigmaL += 3958 * Math.sin(A1)
-  SigmaL += 1962 * Math.sin(Ldash - F)
-  SigmaL += 318 * Math.sin(A2)
+  SigmaL = SigmaL
+    .plus(new Decimal(3958).mul(Decimal.sin(A1)))
+    .plus(new Decimal(1962).mul(Decimal.sin(Ldash.minus(F))))
+    .plus(new Decimal(318).mul(Decimal.sin(A2)))
 
   // And finally apply the nutation in longitude
-  const NutationInLong = getNutationInLongitude(jd)
+  const NutationInLong = getNutationInLongitude(jd).dividedBy(3600)
 
-  const LdashDegrees = Ldash * RAD2DEG
-  return fmod360(LdashDegrees + SigmaL / 1000000 + NutationInLong / 3600)
+  return fmod360(Ldash.mul(RAD2DEG).plus(SigmaL.dividedBy(1000000)).plus(NutationInLong))
 }
 
 /**
@@ -111,45 +119,40 @@ export function getEclipticLongitude (jd: JulianDay): Degree {
  * @param {JulianDay} jd The julian day
  * @returns {Degree}
  */
-export function getEclipticLatitude (jd: JulianDay): Degree {
-  const Ldash = getMeanLongitude(jd) * DEG2RAD
-  const D = getMeanElongation(jd) * DEG2RAD
-  const M = getSunMeanAnomaly(jd) * DEG2RAD
-  const Mdash = getMeanAnomaly(jd) * DEG2RAD
-  const F = getArgumentOfLatitude(jd) * DEG2RAD
+export function getEclipticLatitude (jd: JulianDay | number): Degree {
+  const Ldash = getMeanLongitude(jd).mul(DEG2RAD)
+  const D = getMeanElongation(jd).mul(DEG2RAD)
+  const M = Sun.getMeanAnomaly(jd).mul(DEG2RAD)
+  const Mdash = getMeanAnomaly(jd).mul(DEG2RAD)
+  const F = getArgumentOfLatitude(jd).mul(DEG2RAD)
   const E = getEarthEccentricity(jd)
-  const Esquared = E * E
-  const T = (jd - 2451545) / 36525
+  const T = getJulianCentury(jd)
 
-  const A1 = fmod360(119.75 + 131.849 * T) * DEG2RAD
-  const A3 = fmod360(313.45 + 481266.484 * T) * DEG2RAD
+  const A1 = fmod360(new Decimal(119.75).plus(new Decimal(131.849).mul(T))).mul(DEG2RAD)
+  const A3 = fmod360(new Decimal(313.45).plus(new Decimal(481266.484).mul(T))).mul(DEG2RAD)
 
-  let SigmaB = 0
-  for (let i = 0; i < gMoonCoefficients3.length; i++) {
-    let ThisSigma = gMoonCoefficients4[i] *
-      Math.sin(gMoonCoefficients3[i].D * D +
-        gMoonCoefficients3[i].M * M +
-        gMoonCoefficients3[i].Mdash * Mdash +
-        gMoonCoefficients3[i].F * F)
-
-    if ((gMoonCoefficients3[i].M === 1) || (gMoonCoefficients3[i].M === -1)) {
-      ThisSigma *= E
-    } else if ((gMoonCoefficients3[i].M === 2) || (gMoonCoefficients3[i].M === -2)) {
-      ThisSigma *= Esquared
-    }
-
-    SigmaB += ThisSigma
-  }
+  let SigmaB = gMoonCoefficients3.reduce((sum, val, index) => {
+    const argument = val.D.mul(D)
+      .plus(val.M.mul(M))
+      .plus(val.Mdash.mul(Mdash))
+      .plus(val.F.mul(F))
+    const modulator = (val.M.abs().toNumber() === 1) ? E :
+      (val.M.abs().toNumber() === 2) ? E.pow(2)
+        : 1
+    // SigmaB values = unique column of "4" coefficient array: gMoonCoefficients4. See AA p.338 & 339
+    return sum.plus(gMoonCoefficients4[index].mul(modulator).mul(Decimal.sin(argument)))
+  }, new Decimal(0))
 
   // Finally the additive terms
-  SigmaB -= 2235 * Math.sin(Ldash)
-  SigmaB += 382 * Math.sin(A3)
-  SigmaB += 175 * Math.sin(A1 - F)
-  SigmaB += 175 * Math.sin(A1 + F)
-  SigmaB += 127 * Math.sin(Ldash - Mdash)
-  SigmaB -= 115 * Math.sin(Ldash + Mdash)
+  SigmaB = SigmaB
+    .minus(new Decimal(2235).mul(Decimal.sin(Ldash)))
+    .plus(new Decimal(382).mul(Decimal.sin(A3)))
+    .plus(new Decimal(175).mul(Decimal.sin(A1.minus(F))))
+    .plus(new Decimal(175).mul(Decimal.sin(A1.plus(F))))
+    .plus(new Decimal(127).mul(Decimal.sin(Ldash.minus(Mdash))))
+    .minus(new Decimal(115).mul(Decimal.sin(Ldash.plus(Mdash))))
 
-  return SigmaB / 1000000
+  return SigmaB.dividedBy(1000000)
 }
 
 /**
@@ -157,7 +160,7 @@ export function getEclipticLatitude (jd: JulianDay): Degree {
  * @param {JulianDay} jd The julian day
  * @returns {EclipticCoordinates}
  */
-export function getEclipticCoordinates (jd: JulianDay): EclipticCoordinates {
+export function getEclipticCoordinates (jd: JulianDay | number): EclipticCoordinates {
   return {
     longitude: getEclipticLongitude(jd),
     latitude: getEclipticLatitude(jd)
@@ -169,32 +172,26 @@ export function getEclipticCoordinates (jd: JulianDay): EclipticCoordinates {
  * @param {JulianDay} jd The julian day
  * @returns {Kilometer}
  */
-export function getRadiusVector (jd: JulianDay): Kilometer {
-  const D = getMeanElongation(jd) * DEG2RAD
-  const M = getSunMeanAnomaly(jd) * DEG2RAD
-  const Mdash = getMeanAnomaly(jd) * DEG2RAD
-  const F = getArgumentOfLatitude(jd) * DEG2RAD
+export function getRadiusVector (jd: JulianDay | number): Kilometer {
+  const D = getMeanElongation(jd).mul(DEG2RAD)
+  const M = Sun.getMeanAnomaly(jd).mul(DEG2RAD)
+  const Mdash = getMeanAnomaly(jd).mul(DEG2RAD)
+  const F = getArgumentOfLatitude(jd).mul(DEG2RAD)
   const E = getEarthEccentricity(jd)
-  const Esquared = E * E
 
-  let SigmaR = 0
-  for (let i = 0; i < gMoonCoefficients1.length; i++) {
-    let ThisSigma = gMoonCoefficients2[i].B *
-      Math.cos(gMoonCoefficients1[i].D * D +
-        gMoonCoefficients1[i].M * M +
-        gMoonCoefficients1[i].Mdash * Mdash +
-        gMoonCoefficients1[i].F * F)
+  const SigmaR = gMoonCoefficients1.reduce((sum, val, index) => {
+    const argument = val.D.mul(D)
+      .plus(val.M.mul(M))
+      .plus(val.Mdash.mul(Mdash))
+      .plus(val.F.mul(F))
+    const modulator = (val.M.abs().toNumber() === 1) ? E :
+      (val.M.abs().toNumber() === 2) ? E.pow(2)
+        : 1
+    // SigmaR values = "B" column of "2" coefficient array: gMoonCoefficients2. See AA p.338 & 339
+    return sum.plus(gMoonCoefficients2[index].B.mul(modulator).mul(Decimal.cos(argument)))
+  }, new Decimal(0))
 
-    if ((gMoonCoefficients1[i].M === 1) || (gMoonCoefficients1[i].M === -1)) {
-      ThisSigma *= E
-    } else if ((gMoonCoefficients1[i].M === 2) || (gMoonCoefficients1[i].M === -2)) {
-      ThisSigma *= Esquared
-    }
-
-    SigmaR += ThisSigma
-  }
-
-  return 385000.56 + SigmaR / 1000
+  return new Decimal(385000.56).plus(SigmaR.dividedBy(1000))
 }
 
 /**
@@ -202,8 +199,8 @@ export function getRadiusVector (jd: JulianDay): Kilometer {
  * @param {Kilometer} radiusVector The radius vector
  * @returns {Degree}
  */
-export function radiusVectorToHorizontalParallax (radiusVector: Kilometer): Degree {
-  return RAD2DEG * Math.asin(6378.14 / radiusVector)
+export function radiusVectorToHorizontalParallax (radiusVector: Kilometer | number): Degree {
+  return Decimal.asin(new Decimal(6378.14).dividedBy(radiusVector)).mul(RAD2DEG)
 }
 
 /**
@@ -211,8 +208,8 @@ export function radiusVectorToHorizontalParallax (radiusVector: Kilometer): Degr
  * @param {Degree} horizontalParallax
  * @returns {Kilometer}
  */
-export function horizontalParallaxToRadiusVector (horizontalParallax: Degree): Kilometer {
-  return 6378.14 / Math.sin(DEG2RAD * horizontalParallax)
+export function horizontalParallaxToRadiusVector (horizontalParallax: Degree | number): Kilometer {
+  return new Decimal(6378.14).dividedBy(Decimal.sin(new Decimal(DEG2RAD).mul(horizontalParallax)))
 }
 
 /**
@@ -220,12 +217,15 @@ export function horizontalParallaxToRadiusVector (horizontalParallax: Degree): K
  * @param {JulianDay} jd The julian day
  * @returns {Degree}
  */
-export function getMeanLongitudeAscendingNode (jd: JulianDay): Degree {
-  const T = (jd - 2451545) / 36525
-  const T2 = T * T
-  const T3 = T2 * T
-  const T4 = T3 * T
-  return fmod360(125.0445479 - 1934.1362891 * T + 0.0020754 * T2 + T3 / 467441 - T4 / 60616000)
+export function getMeanLongitudeAscendingNode (jd: JulianDay | number): Degree {
+  const T = getJulianCentury(jd)
+  return fmod360(
+    new Decimal(125.0445479)
+      .minus(new Decimal(1934.1362891).mul(T))
+      .plus(new Decimal(0.0020754).mul(T.pow(2)))
+      .plus(T.pow(3).dividedBy(467441))
+      .minus(T.pow(4).dividedBy(60616000))
+  )
 }
 
 /**
@@ -233,12 +233,15 @@ export function getMeanLongitudeAscendingNode (jd: JulianDay): Degree {
  * @param {JulianDay} jd The julian day
  * @returns {Degree}
  */
-export function getMeanLongitudePerigee (jd: JulianDay): Degree {
-  const T = (jd - 2451545) / 36525
-  const T2 = T * T
-  const T3 = T2 * T
-  const T4 = T3 * T
-  return fmod360(83.3532465 + 4069.0137287 * T - 0.0103200 * T2 - T3 / 80053 + T4 / 18999000)
+export function getMeanLongitudePerigee (jd: JulianDay | number): Degree {
+  const T = getJulianCentury(jd)
+  return fmod360(
+    new Decimal(83.3532465)
+      .plus(new Decimal(4069.0137287).mul(T))
+      .minus(new Decimal(0.0103200).mul(T.pow(2)))
+      .minus(T.pow(3).dividedBy(80053))
+      .plus(T.pow(4).dividedBy(18999000))
+  )
 }
 
 /**
@@ -246,20 +249,21 @@ export function getMeanLongitudePerigee (jd: JulianDay): Degree {
  * @param {JulianDay} jd The julian day
  * @returns {Degree}
  */
-export function trueLongitudeAscendingNode (jd: JulianDay): Degree {
+export function trueLongitudeOfAscendingNode (jd: JulianDay | number): Degree {
   let TrueAscendingNode = getMeanLongitudeAscendingNode(jd)
 
-  const D = getMeanElongation(jd) * DEG2RAD
-  const M = getSunMeanAnomaly(jd) * DEG2RAD
-  const Mdash = getMeanAnomaly(jd) * DEG2RAD
-  const F = getArgumentOfLatitude(jd) * DEG2RAD
+  const D = getMeanElongation(jd).mul(DEG2RAD)
+  const M = Sun.getMeanAnomaly(jd).mul(DEG2RAD)
+  const Mdash = getMeanAnomaly(jd).mul(DEG2RAD)
+  const F = getArgumentOfLatitude(jd).mul(DEG2RAD)
 
   // Add the principal additive terms
-  TrueAscendingNode -= 1.4979 * Math.sin(2 * (D - F))
-  TrueAscendingNode -= 0.1500 * Math.sin(M)
-  TrueAscendingNode -= 0.1226 * Math.sin(2 * D)
-  TrueAscendingNode += 0.1176 * Math.sin(2 * F)
-  TrueAscendingNode -= 0.0801 * Math.sin(2 * (Mdash - F))
+  TrueAscendingNode = TrueAscendingNode
+    .minus(new Decimal(1.4979).mul(Decimal.sin(TWO.mul(D.minus(F)))))
+    .minus(new Decimal(0.1500).mul(Decimal.sin(M)))
+    .minus(new Decimal(0.1226).mul(Decimal.sin(TWO.mul(D))))
+    .plus(new Decimal(0.1176).mul(Decimal.sin(TWO.mul(F))))
+    .minus(new Decimal(0.0801).mul(Decimal.sin(TWO.mul(Mdash.minus(F)))))
 
   return fmod360(TrueAscendingNode)
 }
@@ -269,17 +273,17 @@ export function trueLongitudeAscendingNode (jd: JulianDay): Degree {
  * @param {JulianDay} jd The julian day
  * @return {Degree}
  */
-export function horizontalParallax (jd: JulianDay): Degree {
+export function horizontalParallax (jd: JulianDay | number): Degree {
   return radiusVectorToHorizontalParallax(getRadiusVector(jd))
 }
 
 /**
- * Equatorial coordinates
+ * Heliocentric equatorial coordinates
  * @see getApparentEquatorialCoordinates
  * @param {JulianDay} jd The julian day
  * @returns {EquatorialCoordinates}
  */
-export function getEquatorialCoordinates (jd: JulianDay): EquatorialCoordinates {
+export function getEquatorialCoordinates (jd: JulianDay | number): EquatorialCoordinates {
   return transformEclipticToEquatorial(
     getEclipticLongitude(jd),
     getEclipticLatitude(jd),
@@ -288,12 +292,12 @@ export function getEquatorialCoordinates (jd: JulianDay): EquatorialCoordinates 
 }
 
 /**
- * Apparent equatorial coordinates (using the true obliquity of the ecliptic)
+ * Apparent heliocentric equatorial coordinates (using the true obliquity of the ecliptic)
  * @see getTrueObliquityOfEcliptic
  * @param {JulianDay} jd The julian day
  * @returns {EquatorialCoordinates}
  */
-export function getApparentEquatorialCoordinates (jd: JulianDay): EquatorialCoordinates {
+export function getApparentEquatorialCoordinates (jd: JulianDay | number): EquatorialCoordinates {
   return transformEclipticToEquatorial(
     getEclipticLongitude(jd),
     getEclipticLatitude(jd),
