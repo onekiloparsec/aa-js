8f1feb720377ccb718039303fa11310eae045126
Cédric Foellmi <cedric@onekiloparsec.dev>
2023-11-01 22:20:39 +0100

    Improved tests
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- tests/venus.test.js
+++ tests/venus.test.js
@@ -1,4 +1,5 @@
-import { constants, juliandays, times, Venus } from '../src'
+import { constants, juliandays, times, Venus } from '@'
+import { getDecimalYear, getFractionalYear } from '@/dates'
 
 describe('venus', () => {
   test('check that polar and equatorial semi diameters are identical', () => {
@@ -19,11 +20,19 @@ describe('venus', () => {
 
 
   // See AA p.103, Example 15.a
-  test.skip('check apparent equatorial coordinates of Venus', () => {
+  test('check apparent equatorial coordinates of Venus', () => {
     const jd = juliandays.getJulianDay(new Date(Date.UTC(1988, 2, 20, 0, 0, 0)))
     expect(jd.toNumber()).toEqual(2447240.5)
-    const coords = Venus.getApparentEquatorialCoordinates(times.transformUTC2TT(jd))
-    expect(coords.rightAscension.toNumber() * constants.H2DEG).toBeCloseTo(41.73129, 4)
-    expect(coords.declination.toNumber()).toBeCloseTo(18.44092, 4)
+    const coords = Venus.getApparentGeocentricEquatorialCoordinates(times.transformUTC2TT(jd))
+    expect(coords.rightAscension.toNumber() * constants.H2DEG).toBeCloseTo(41.73129, 2)
+    expect(coords.declination.toNumber()).toBeCloseTo(18.44092, 2)
+  })
+
+  // See AA p270, Ex. 38.a
+  test('check date of perihelion', () => {
+    const jd = juliandays.getJulianDay(new Date(Date.UTC(1978, 9, 15)))
+    const decimalYear = getDecimalYear(jd)
+    expect(decimalYear.toNumber()).toBeCloseTo(1978.79, 2)
+    expect(Venus.getPerihelion(jd).toNumber()).toBeCloseTo(2443_873.704, 3)
   })
 })
