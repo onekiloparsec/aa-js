bfe6e380f1f702395228f2fb7503668de88219d4
Cédric Foellmi <cedric@onekiloparsec.dev>
2023-11-02 14:14:13 +0100

    Using new method name
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- src/planets/risetransitsets.ts
+++ src/planets/risetransitsets.ts
@@ -4,7 +4,7 @@ import * as risetransitsets from '@/risetransitsets'
 
 export function getPlanetRiseTransitSet (jd: JulianDay | number, geoCoords: GeographicCoordinates, getCoordsFunc: EquatorialCoordinatesAtJulianDayFunction): RiseTransitSet {
   const coords = getCoordsFunc(new Decimal(jd))
-  return risetransitsets.getRiseSetTransitTimes(
+  return risetransitsets.getRiseTransitSetTimes(
     jd,
     coords.rightAscension,
     coords.declination,
@@ -17,7 +17,7 @@ export function getPlanetAccurateRiseTransitSet (jd: JulianDay | number, geoCoor
   const coords0 = getCoordsFunc(new Decimal(jd).minus(1))
   const coords1 = getCoordsFunc(new Decimal(jd))
   const coords2 = getCoordsFunc(new Decimal(jd).plus(1))
-  return risetransitsets.getAccurateRiseSetTransitTimes(
+  return risetransitsets.getAccurateRiseTransitSetTimes(
     jd,
     [coords0.rightAscension, coords1.rightAscension, coords2.rightAscension],
     [coords0.declination, coords1.declination, coords2.declination],
