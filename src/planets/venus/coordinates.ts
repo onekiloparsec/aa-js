df1b5a07d6a8c690205bb8fcf8c5da8063db979f
Cédric Foellmi <cedric@onekiloparsec.dev>
2023-11-02 11:09:19 +0100

    Linting & usine conversions fonctions
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- src/planets/venus/coordinates.ts
+++ src/planets/venus/coordinates.ts
@@ -48,7 +48,7 @@ export function getEclipticLongitude (jd: JulianDay | number): Degree {
   )
     .dividedBy(1e8)
 
-  return fmod360(value.mul(RAD2DEG))
+  return fmod360(value.radiansToDegrees())
 }
 
 /**
@@ -73,7 +73,7 @@ export function getEclipticLatitude (jd: JulianDay | number): Degree {
   )
     .dividedBy(1e8)
 
-  return fmod90(value.mul(RAD2DEG))
+  return fmod90(value.radiansToDegrees())
 }
 
 /**
