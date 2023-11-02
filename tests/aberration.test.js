3da5bf7823c58a3ac731f665c76cf7a1959b72ed
Cédric Foellmi <cedric@onekiloparsec.dev>
2023-11-02 14:14:09 +0100

    Better tests
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- tests/aberration.test.js
+++ tests/aberration.test.js
@@ -1,4 +1,3 @@
-import Decimal from '@/decimal'
 import { getJulianDay } from '@/juliandays'
 import { getAnnualEquatorialAberration, getNutationEquatorialAberration } from '@/earth/aberration'
 import { getNutationInLongitude, getNutationInObliquity } from '@/earth/nutation'
@@ -8,7 +7,7 @@ import { getDecimalValue } from '@/sexagesimal'
 
 describe('aberration', () => {
   // See AA p152 & 153, Ex 23.a
-  test('corrections for theta Persei', () => {
+  test('nutation equatorial aberration corrections for theta Persei', () => {
     // The date is strangely expressed in the book.
     const utcDate = new Date(Date.UTC(2028, 10, 13, 19 / 24.))
     const jd = transformUTC2TT(getJulianDay(utcDate))
@@ -28,7 +27,14 @@ describe('aberration', () => {
     const nutation = getNutationEquatorialAberration(jd, coords.rightAscension, coords.declination)
     expect(nutation.DeltaRightAscension.toNumber()).toBeCloseTo(15.843, 2)
     expect(nutation.DeltaDeclination.toNumber()).toBeCloseTo(6.218, 2)
+  })
 
+  // See AA p152 & 153, Ex 23.a
+  test('annual equatorial aberration corrections for theta Persei', () => {
+    // The date is strangely expressed in the book.
+    const utcDate = new Date(Date.UTC(2028, 10, 13, 19 / 24.))
+    const jd = transformUTC2TT(getJulianDay(utcDate))
+    const coords = { rightAscension: getDecimalValue(2, 46, 11.331), declination: getDecimalValue(49, 20, 54.54) }
     const annual = getAnnualEquatorialAberration(jd, coords.rightAscension, coords.declination)
     expect(annual.DeltaRightAscension.toNumber()).toBeCloseTo(30.045, 2)
     expect(annual.DeltaDeclination.toNumber()).toBeCloseTo(6.697, 1)
