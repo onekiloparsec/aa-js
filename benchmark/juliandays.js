07efb5c26ab63347e8d27033223cb1fb63595fb6
Cédric Foellmi <cedric@onekiloparsec.dev>
2023-11-12 10:21:35 +0100

    Introducing benchmark tests!
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

---
+++ benchmark/juliandays.js
@@ -0,0 +1,20 @@
+import * as b from 'benny'
+import * as juliandays from '../src/juliandays.ts'
+
+const jd = juliandays.getJulianDay()
+
+b.suite(
+  'juliandays',
+
+  b.add('getLocalSiderealTime high', () => {
+    juliandays.getLocalSiderealTime(jd, 10)
+  }),
+  b.add('getLocalSiderealTime low', () => {
+    juliandays.getLocalSiderealTime(jd, 10, false)
+  }),
+
+  b.cycle(),
+  b.complete(),
+  b.save({ file: 'juliandays', version: '1.0.0' }),
+  b.save({ file: 'juliandays', format: 'chart.html' })
+)
