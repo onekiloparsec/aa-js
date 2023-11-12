dc2ddf0e9d0160f39c725f89a0098b0f65a67c0f
Cédric Foellmi <cedric@onekiloparsec.dev>
2023-11-12 14:59:46 +0100

    Better types for Moon
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- src/types/funcs.ts
+++ src/types/funcs.ts
@@ -4,23 +4,34 @@ import { EclipticCoordinates, EquatorialCoordinates, GeographicCoordinates } fro
 import { RiseTransitSet } from './risetransitset'
 import { Equinox } from './earth'
 
+export type JulianDayForJulianDayFunction = (jd: JulianDay | number) => JulianDay
+
 export type SingleCoordinateDegreeAtJulianDayFunction = (jd: JulianDay | number) => Degree
 export type EclipticCoordinatesAtJulianDayFunction = (jd: JulianDay | number) => EclipticCoordinates
 export type EquatorialCoordinatesAtJulianDayFunction = (jd: JulianDay | number) => EquatorialCoordinates
-export type JulianDayForJulianDayFunction = (jd: JulianDay | number) => JulianDay
 
+export type SingleCoordinateDegreeAtJulianDayWithPrecisionFunction = (jd: JulianDay | number, equinox?: Equinox) => Degree
 export type SingleCoordinateDegreeWithEquinoxAtJulianDayFunction = (jd: JulianDay | number, equinox?: Equinox) => Degree
+
+export type EclipticCoordinatesAtJulianDayWithPrecisionFunction = (jd: JulianDay | number, equinox?: Equinox) => EclipticCoordinates
 export type EclipticCoordinatesWithEquinoxAtJulianDayFunction = (jd: JulianDay | number, equinox?: Equinox) => EclipticCoordinates
+
+export type EquatorialCoordinatesAtJulianDayWithPrecisionFunction = (jd: JulianDay | number, equinox?: Equinox) => EquatorialCoordinates
 export type EquatorialCoordinatesWithEquinoxAtJulianDayFunction = (jd: JulianDay | number, equinox?: Equinox) => EquatorialCoordinates
 
 export type QuantityAtJulianDayFunction = (jd: JulianDay | number) => Decimal
 export type QuantityAtJulianDayWithPrecisionFunction = (jd: JulianDay | number, highPrecision?: boolean) => Decimal
+
 export type QuantityInDegreeAtJulianDayFunction = (jd: JulianDay | number) => Degree
 export type QuantityInDegreeAtJulianDayWithPrecisionFunction = (jd: JulianDay | number, highPrecision?: boolean) => Degree
+
 export type QuantityInDegreeAtJulianCenturyFunction = (jd: JulianCentury | number) => Degree
 export type QuantityInDegreeAtJulianCenturyWithPrecisionFunction = (jd: JulianCentury | number, highPrecision?: boolean) => Degree
-export type QuantityInMagnitudeAtJulianDayFunction = (jd: JulianDay | number) => Magnitude
+
 export type QuantityInAstronomicalUnitAtJulianDayFunction = (jd: JulianDay | number) => AstronomicalUnit
+export type QuantityInAstronomicalUnitAtJulianDayWithPrecisionFunction = (jd: JulianDay | number, highPrecision?: boolean) => AstronomicalUnit
+
+export type QuantityInMagnitudeAtJulianDayFunction = (jd: JulianDay | number) => Magnitude
 export type QuantityInKilometerPerSecondAtJulianDayFunction = (jd: JulianDay | number) => KilometerPerSecond
 export type QuantityInArcSecondAtJulianDayFunction = (jd: JulianDay | number) => ArcSecond
 
