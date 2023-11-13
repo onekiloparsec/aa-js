fa772fb0050b4a68ab98af17e733caced3819704
Cédric Foellmi <cedric@onekiloparsec.dev>
2023-11-13 21:44:00 +0100

    Usong correct return type
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- src/sun/sun.ts
+++ src/sun/sun.ts
@@ -2,7 +2,7 @@
  @module Sun
  */
 import Decimal from '@/decimal'
-import { Degree, EclipticCoordinates, EquatorialCoordinatesH, Equinox, JulianCentury, JulianDay } from '@/types'
+import { Degree, EclipticCoordinates, EquatorialCoordinates, Equinox, JulianCentury, JulianDay } from '@/types'
 import { DEG2RAD } from '@/constants'
 import { getJulianCentury } from '@/juliandays'
 import { transformEclipticToEquatorial } from '@/coordinates'
@@ -191,9 +191,9 @@ export function getGeocentricEclipticCoordinates (jd: JulianDay | number, equino
  * Geocentric equatorial coordinates
  * @param {JulianDay} jd The julian day
  * @param {Equinox} equinox (optional) The equinox to be used: MeanOfTheDate (default) or StandardJ2000.
- * @returns EquatorialCoordinatesH
+ * @returns EquatorialCoordinates
  */
-export function getGeocentricEquatorialCoordinates (jd: JulianDay | number, equinox: Equinox = Equinox.MeanOfTheDate): EquatorialCoordinatesH {
+export function getGeocentricEquatorialCoordinates (jd: JulianDay | number, equinox: Equinox = Equinox.MeanOfTheDate): EquatorialCoordinates {
   return transformEclipticToEquatorial(
     getGeocentricEclipticCoordinates(jd, equinox),
     Earth.getMeanObliquityOfEcliptic(jd)
