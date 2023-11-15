9d5dbd5e4dc6a342e845a7bc5dd0f58715dc25b1
Cédric Foellmi <cedric@onekiloparsec.dev>
2023-11-15 22:08:44 +0100

    Correcting the call to risetransitset.getRiseTransitSetTimes and adding highprecision flag
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- src/planets/risetransitset.ts
+++ src/planets/risetransitset.ts
@@ -1,29 +1,18 @@
 import Decimal from '@/decimal'
 import { EquatorialCoordinatesAtJulianDayFunction, GeographicCoordinates, JulianDay, RiseTransitSet } from '@/types'
 import * as risetransitset from '@/risetransitset'
+import { STANDARD_ALTITUDE_STARS } from '@/constants'
 
 /** @private */
-export function getPlanetRiseTransitSet (jd: JulianDay | number, geoCoords: GeographicCoordinates, getCoordsFunc: EquatorialCoordinatesAtJulianDayFunction): RiseTransitSet {
+export function getPlanetRiseTransitSet (jd: JulianDay | number, geoCoords: GeographicCoordinates, getCoordsFunc: EquatorialCoordinatesAtJulianDayFunction, highPrecision: boolean = true): RiseTransitSet {
   const coords = getCoordsFunc(new Decimal(jd))
-  return risetransitset.getRiseTransitSetTimes(
-    jd,
-    coords.rightAscension,
-    coords.declination,
-    geoCoords.longitude,
-    geoCoords.latitude
-  )
+  return risetransitset.getRiseTransitSetTimes(jd, coords, geoCoords, STANDARD_ALTITUDE_STARS, highPrecision)
 }
 
 /** @private */
-export function getPlanetAccurateRiseTransitSet (jd: JulianDay | number, geoCoords: GeographicCoordinates, getCoordsFunc: EquatorialCoordinatesAtJulianDayFunction): RiseTransitSet {
+export function getPlanetAccurateRiseTransitSet (jd: JulianDay | number, geoCoords: GeographicCoordinates, getCoordsFunc: EquatorialCoordinatesAtJulianDayFunction, highPrecision: boolean = true): RiseTransitSet {
   const coords0 = getCoordsFunc(new Decimal(jd).minus(1))
   const coords1 = getCoordsFunc(new Decimal(jd))
   const coords2 = getCoordsFunc(new Decimal(jd).plus(1))
-  return risetransitset.getAccurateRiseTransitSetTimes(
-    jd,
-    [coords0.rightAscension, coords1.rightAscension, coords2.rightAscension],
-    [coords0.declination, coords1.declination, coords2.declination],
-    geoCoords.longitude,
-    geoCoords.latitude
-  )
+  return risetransitset.getAccurateRiseTransitSetTimes(jd, [coords0, coords1, coords2], geoCoords, STANDARD_ALTITUDE_STARS, 1, highPrecision)
 }
