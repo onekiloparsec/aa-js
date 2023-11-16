f3e2e054415526c207a1e98fca8c15757ec22f51
Cédric Foellmi <cedric@onekiloparsec.dev>
2023-11-16 21:22:17 +0100

    Consistency
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- src/planets/risetransitset.ts
+++ src/planets/risetransitset.ts
@@ -1,12 +1,12 @@
 import Decimal from '@/decimal'
 import { EquatorialCoordinatesAtJulianDayFunction, GeographicCoordinates, JulianDay, RiseTransitSet } from '@/types'
-import * as risetransitset from '@/risetransitset'
+import { getAccurateRiseTransitSetTimes, getRiseTransitSetTimes } from '@/risetransitset'
 import { STANDARD_ALTITUDE_STARS } from '@/constants'
 
 /** @private */
 export function getPlanetRiseTransitSet (jd: JulianDay | number, geoCoords: GeographicCoordinates, getCoordsFunc: EquatorialCoordinatesAtJulianDayFunction, highPrecision: boolean = true): RiseTransitSet {
   const coords = getCoordsFunc(new Decimal(jd))
-  return risetransitset.getRiseTransitSetTimes(jd, coords, geoCoords, STANDARD_ALTITUDE_STARS, highPrecision)
+  return getRiseTransitSetTimes(jd, coords, geoCoords, STANDARD_ALTITUDE_STARS, highPrecision)
 }
 
 /** @private */
@@ -14,5 +14,5 @@ export function getPlanetAccurateRiseTransitSet (jd: JulianDay | number, geoCoor
   const coords0 = getCoordsFunc(new Decimal(jd).minus(1))
   const coords1 = getCoordsFunc(new Decimal(jd))
   const coords2 = getCoordsFunc(new Decimal(jd).plus(1))
-  return risetransitset.getAccurateRiseTransitSetTimes(jd, [coords0, coords1, coords2], geoCoords, STANDARD_ALTITUDE_STARS, 1, highPrecision)
+  return getAccurateRiseTransitSetTimes(jd, [coords0, coords1, coords2], geoCoords, STANDARD_ALTITUDE_STARS, 1, highPrecision)
 }
