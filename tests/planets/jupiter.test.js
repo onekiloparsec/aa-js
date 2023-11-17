883851669a6fe65621f574a485c42d908d69730e
Cédric Foellmi <cedric@onekiloparsec.dev>
2023-11-17 08:47:31 +0100

    Adding some tests
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- tests/planets/jupiter.test.js
+++ tests/planets/jupiter.test.js
@@ -1,8 +1,22 @@
 import { Jupiter } from '@/planets'
+import * as juliandays from '@/juliandays'
+import { transformUTC2TT } from '@/times'
 
-test('check physical details', () => {
-  const jd = 2448972.50068
-  const apparentDiameter = Jupiter.getPolarSemiDiameter(jd)
-  expect(apparentDiameter).toBeCloseTo(16.23, 1) // arcsec
+describe('Jupiter', () => {
+  test('check apparent diameter', () => {
+    const jd = 2448972.50068
+    const apparentDiameter = Jupiter.getPolarSemiDiameter(jd)
+    expect(apparentDiameter).toBeCloseTo(16.23, 1) // arcsec
+  })
+
+  // See AA p295, Ex 43.a
+  test('check physical details', () => {
+    const UTCDate = new Date(Date.UTC(1992, 11, 16))
+    const jd = juliandays.getJulianDay(UTCDate)
+    expect(jd.toNumber()).toEqual(2448972.5)
+    const jd0 = transformUTC2TT(jd)
+    expect(jd0.toNumber()).toEqual(2448972.500685)
+    // TODO: rewrite Jupiter details based on what is done for Saturn ring system.
+  })
 })
 
--- tests/planets/mars.test.js
+++ tests/planets/mars.test.js
@@ -1,31 +1,21 @@
 import { Mars } from '@/planets'
 import * as juliandays from '@/juliandays'
 
+describe('Mars', () => {
+  // See AA p.291, Example 42.a, see also SwiftAA
+  test('check physical details', () => {
+    const UTCDate = new Date(Date.UTC(1992, 10, 9))
+    const jd = juliandays.getJulianDay(UTCDate)
 
-// See AA p.291, Example 42.a, see also SwiftAA
-test('check physical details', () => {
-  const UTCDate = new Date(Date.UTC(1992, 10, 9))
-  const jd = juliandays.getJulianDay(UTCDate)
+    const earthDec = Mars.getPlanetocentricDeclinationOfTheEarth(jd)
+    expect(earthDec.toNumber()).toBeCloseTo(12.44)// deg
 
-  const earthDec = Mars.getPlanetocentricDeclinationOfTheEarth(jd)
-  expect(earthDec.toNumber()).toBeCloseTo(12.44)// deg
+    const sunDec = Mars.getPlanetocentricDeclinationOfTheSun(jd)
+    expect(sunDec.toNumber()).toBeCloseTo(-2.76)// deg
+  })
 
-  const sunDec = Mars.getPlanetocentricDeclinationOfTheSun(jd)
-  expect(sunDec.toNumber()).toBeCloseTo(-2.76)// deg
-
-  // const posAngle = mars.positionAngleOfNorthernRotationPole(jd)
-  // expect(posAngle).toBeCloseTo(347.64)// deg
-  //
-  // const longCentralMeridian = mars.aerographicLongitudeOfCentralMeridian(jd)
-  // expect(longCentralMeridian).toBeCloseTo(111.55)// deg
-  //
-  // const angDefectIllum = mars.angularAmountOfGreatestDefectOfIllumination(jd)
-  // expect(angDefectIllum).toBeCloseTo(1.06) // arcsec
-  //
-  // const posDefectIllum = mars.positionAngleOfGreatestDefectOfIllumination(jd)
-  // expect(posDefectIllum).toBeCloseTo(279.91) // deg
-
-  const apparentDiameter = Mars.getEquatorialSemiDiameter(jd)
-  expect(apparentDiameter.toNumber()).toBeCloseTo(10.75 / 2) // arcsec
+  test('check apparent diameter', () => {
+    const apparentDiameter = Mars.getEquatorialSemiDiameter(jd)
+    expect(apparentDiameter.toNumber()).toBeCloseTo(10.75 / 2) // arcsec
+  })
 })
-
