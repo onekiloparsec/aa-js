79b76558de47ba62a9cce4de652f77af139495dd
Cédric Foellmi <cedric@onekiloparsec.dev>
2023-11-18 17:49:19 +0100

    Adding new benchmarks
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- benchmark/trigonometrics.js
+++ benchmark/trigonometrics.js
@@ -1,8 +1,9 @@
 import * as b from 'benny'
-import { Decimal } from '@'
+import { Decimal, DEG2RAD } from '@'
 
 const value = new Decimal(Math.random())
 const valueNum = value.toNumber()
+const deg2rad = DEG2RAD.toNumber()
 
 export default b.suite(
   'trigonometrics decimals numbers',
@@ -18,6 +19,16 @@ export default b.suite(
   }),
   b.add('Math.cos with number value', () => {
     Math.cos(valueNum)
+  }),
+
+  b.add('value deg2rad decimals then toNumber', () => {
+    value.degreesToRadians().toNumber()
+  }),
+  b.add('value toNumber then deg2rad num', () => {
+    value.toNumber() * deg2rad
+  }),
+  b.add('valueNum deg2rad', () => {
+    valueNum * deg2rad
   }),
 
   b.cycle(),
