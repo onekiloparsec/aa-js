f10de623c659fefd41146b2fad66beea7e0e5eca
Cédric Foellmi <cedric@onekiloparsec.dev>
2023-11-18 16:21:19 +0100

    Making it a lot faster! From 750 ops/s to 1 150 000!
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- benchmark/coordinates/great-circle-angular-distance.js
+++ benchmark/coordinates/great-circle-angular-distance.js
@@ -5,7 +5,7 @@ const thetaPersei = { rightAscension: 41.511331, declination: 49.3545 }
 const coordsVenus = { rightAscension: 41.73129, declination: 18.44092 }
 
 b.suite(
-  'coordinates',
+  'coordinates getGreatCircleAngularDistance',
 
   b.add('getGreatCircleAngularDistance high', () => {
     coordinates.getGreatCircleAngularDistance(thetaPersei, coordsVenus)
--- src/coordinates.ts
+++ src/coordinates.ts
@@ -23,7 +23,7 @@ import {
   Radian,
   TopocentricCoordinates
 } from './types'
-import { ECLIPTIC_OBLIQUITY_J2000_0, J2000, JULIAN_DAY_B1950_0, MINUSONE, RAD2DEG } from '@/constants'
+import { DEG2RAD, ECLIPTIC_OBLIQUITY_J2000_0, J2000, JULIAN_DAY_B1950_0, MINUSONE, RAD2DEG } from '@/constants'
 import { getLocalSiderealTime } from '@/juliandays'
 import { precessEquatorialCoordinates } from '@/precession'
 import { getFlatteningCorrections } from '@/earth/coordinates'
@@ -603,11 +603,12 @@ export function getParallacticAngle (jd: JulianDay | number, equCoords: Equatori
  * @returns {Degree}
  */
 export function getGreatCircleAngularDistance (coords1: EquatorialCoordinates | EquatorialCoordinatesNum, coords2: EquatorialCoordinates | EquatorialCoordinatesNum, highPrecision: boolean = true) {
-  const alpha1 = new Decimal(coords1.rightAscension).degreesToRadians()
-  const alpha2 = new Decimal(coords2.rightAscension).degreesToRadians()
-  const delta1 = new Decimal(coords1.declination).degreesToRadians()
-  const delta2 = new Decimal(coords2.declination).degreesToRadians()
   if (highPrecision) {
+    const alpha1 = new Decimal(coords1.rightAscension).degreesToRadians()
+    const alpha2 = new Decimal(coords2.rightAscension).degreesToRadians()
+    const delta1 = new Decimal(coords1.declination).degreesToRadians()
+    const delta2 = new Decimal(coords2.declination).degreesToRadians()
+
     const x = Decimal.cos(delta1).mul(Decimal.sin(delta2))
       .minus(Decimal.sin(delta1).mul(Decimal.cos(delta2)).mul(Decimal.cos(alpha2.minus(alpha1))))
     const y = Decimal.cos(delta2).mul(Decimal.sin(alpha2.minus(alpha1)))
@@ -615,11 +616,20 @@ export function getGreatCircleAngularDistance (coords1: EquatorialCoordinates | 
       .plus(Decimal.cos(delta1).mul(Decimal.cos(delta2)).mul(Decimal.cos(alpha2.minus(alpha1))))
     return Decimal.atan2(Decimal.sqrt(x.pow(2).plus(y.pow(2))), z).radiansToDegrees()
   } else {
-    const x = Math.cos(delta1.toNumber()) * Math.sin(delta2.toNumber())
-      - Math.sin(delta1.toNumber()) * Math.cos(delta2.toNumber()) * Math.cos(alpha2.toNumber() - alpha1.toNumber())
-    const y = Math.cos(delta2.toNumber()) * Math.sin(alpha2.toNumber() - alpha1.toNumber())
-    const z = Math.sin(delta1.toNumber()) * Math.sin(delta2.toNumber())
-      + Math.cos(delta1.toNumber()) * Math.cos(delta2.toNumber()) * Math.cos(alpha2.toNumber() - alpha1.toNumber())
-    return Decimal.atan2(Math.sqrt(x * x + y * y), z).radiansToDegrees()
+    const deg2rad = DEG2RAD.toNumber()
+    const alpha1 = (Decimal.isDecimal(coords1.rightAscension) ? coords1.rightAscension.toNumber() : coords1.rightAscension) * deg2rad
+    const alpha2 = (Decimal.isDecimal(coords2.rightAscension) ? coords2.rightAscension.toNumber() : coords2.rightAscension) * deg2rad
+    const delta1 = (Decimal.isDecimal(coords1.declination) ? coords1.declination.toNumber() : coords1.declination) * deg2rad
+    const delta2 = (Decimal.isDecimal(coords2.declination) ? coords2.declination.toNumber() : coords2.declination) * deg2rad
+
+    const x = Math.cos(delta1) * Math.sin(delta2)
+      - Math.sin(delta1) * Math.cos(delta2) * Math.cos(alpha2 - alpha1)
+    const y = Math.cos(delta2) * Math.sin(alpha2 - alpha1)
+    const z = Math.sin(delta1) * Math.sin(delta2)
+      + Math.cos(delta1) * Math.cos(delta2) * Math.cos(alpha2 - alpha1)
+    
+    return new Decimal(
+      Math.atan2(Math.sqrt(x * x + y * y), z) * deg2rad
+    )
   }
 }
