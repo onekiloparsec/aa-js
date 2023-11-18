7006315792ffb5043b45b3bf936510e0b9354a75
Cédric Foellmi <cedric@onekiloparsec.dev>
2023-11-18 18:29:59 +0100

    Refactor
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- src/earth/aberration/aberration.ts
+++ src/earth/aberration/aberration.ts
@@ -1,7 +1,6 @@
 import Decimal from '@/decimal'
 import {
   ArcSecond,
-  Coordinates3D,
   EclipticCoordinates,
   EclipticCoordinatesCorrection,
   EquatorialCoordinates,
@@ -9,111 +8,16 @@ import {
   JulianDay,
   Radian
 } from '@/types'
-import { CONSTANT_OF_ABERRATION, MINUSONE, ZERO } from '@/constants'
-import { getJulianCentury } from '@/juliandays'
+import { CONSTANT_OF_ABERRATION, DEG2RAD, MINUSONE } from '@/constants'
 import { Sun } from '@/sun'
-import { getEccentricity, getLongitudeOfPerihelion } from '../coordinates'
-import { AberrationCoefficient, AberrationCoefficientNum, getAberrationCoefficients } from './coefficients'
 import {
   getMeanObliquityOfEcliptic,
   getNutationInLongitude,
   getNutationInObliquity,
   getTrueObliquityOfEcliptic
 } from '../nutation'
-
-
-/**
- * Earth velocity (as a 3D vector), with respect to the barycenter of the solar system, in the equatorial
- * J2000.0 reference frame.
- * @param {JulianDay} jd The julian day
- * @param {boolean} highPrecision Use (slower) arbitrary-precision decimal computations. default = true.
- * @return {Coordinates3D} The units are 10^-8 AstronomicalUnit per day.
- * @memberof module:Earth
- */
-export function getEarthVelocity (jd: JulianDay | number, highPrecision: boolean = true): Coordinates3D {
-  const T = getJulianCentury(jd)
-  const coefficients = getAberrationCoefficients(highPrecision)
-
-  if (highPrecision) {
-    const L2 = new Decimal('3.176_1467').plus(new Decimal('1021.328_5546').mul(T))
-    const L3 = new Decimal('1.753_4703').plus(new Decimal('628.307_5849').mul(T))
-    const L4 = new Decimal('6.203_4809').plus(new Decimal('334.061_2431').mul(T))
-    const L5 = new Decimal('0.599_5465').plus(new Decimal('52.969_0965').mul(T))
-    const L6 = new Decimal('0.874_0168').plus(new Decimal('21.329_9095').mul(T))
-    const L7 = new Decimal('5.481_2939').plus(new Decimal('7.478_1599').mul(T))
-    const L8 = new Decimal('5.311_8863').plus(new Decimal('3.813_3036').mul(T))
-    const Ldash = new Decimal('3.810_3444').plus(new Decimal('8399.684_7337').mul(T))
-    const D = new Decimal('5.198_4667').plus(new Decimal('7771.377_1486').mul(T))
-    const Mdash = new Decimal('2.355_5559').plus(new Decimal('8328.691_4289').mul(T))
-    const F = new Decimal('1.627_9052').plus(new Decimal('8433.466_1601').mul(T))
-
-    return (coefficients as AberrationCoefficient[]).reduce((sum, val) => {
-      const argument = val.L2.mul(L2)
-        .plus(val.L3.mul(L3))
-        .plus(val.L4.mul(L4))
-        .plus(val.L5.mul(L5))
-        .plus(val.L6.mul(L6))
-        .plus(val.L7.mul(L7))
-        .plus(val.L8.mul(L8))
-        .plus(val.Ldash.mul(Ldash))
-        .plus(val.D.mul(D))
-        .plus(val.Mdash.mul(Mdash))
-        .plus(val.F.mul(F))
-      return {
-        X: sum.X
-          .plus(val.xsin.plus(val.xsint.mul(T)).mul(argument.sin()))
-          .plus(val.xcos.plus(val.xcost.mul(T)).mul(argument.cos())),
-        Y: sum.Y
-          .plus(val.ysin.plus(val.ysint.mul(T)).mul(argument.sin()))
-          .plus(val.ycos.plus(val.ycost.mul(T)).mul(argument.cos())),
-        Z: sum.Z
-          .plus(val.zsin.plus(val.zsint.mul(T)).mul(argument.sin()))
-          .plus(val.zcos.plus(val.zcost.mul(T)).mul(argument.cos()))
-      }
-    }, { X: ZERO, Y: ZERO, Z: ZERO })
-  } else {
-    const tnum = T.toNumber()
-    const L2 = 3.176_1467 + 1021.328_5546 * tnum
-    const L3 = 1.753_4703 + 628.307_5849 * tnum
-    const L4 = 6.203_4809 + 334.061_2431 * tnum
-    const L5 = 0.599_5465 + 52.969_0965 * tnum
-    const L6 = 0.874_0168 + 21.329_9095 * tnum
-    const L7 = 5.481_2939 + 7.478_1599 * tnum
-    const L8 = 5.311_8863 + 3.813_3036 * tnum
-    const Ldash = 3.810_3444 + 8399.684_7337 * tnum
-    const D = 5.198_4667 + 7771.377_1486 * tnum
-    const Mdash = 2.355_5559 + 8328.691_4289 * tnum
-    const F = 1.627_9052 + 8433.466_1601 * tnum
-
-    return (coefficients as AberrationCoefficientNum[]).reduce((sum, val) => {
-      const argument = val.L2 * L2
-        + val.L3 * L3
-        + val.L4 * L4
-        + val.L5 * L5
-        + val.L6 * L6
-        + val.L7 * L7
-        + val.L8 * L8
-        + val.Ldash * Ldash
-        + val.D * D
-        + val.Mdash * Mdash
-        + val.F * F
-      return {
-        X: sum.X.plus(
-          val.xsin + val.xsint * tnum * Math.sin(argument)
-          + val.xcos + val.xcost * tnum * Math.cos(argument)
-        ),
-        Y: sum.Y.plus(
-          val.ysin + val.ysint * tnum * Math.sin(argument)
-          + val.ycos + val.ycost * tnum * Math.cos(argument)
-        ),
-        Z: sum.Z.plus(
-          val.zsin + val.zsint * tnum * Math.sin(argument)
-          + val.zcos + val.zcost * tnum * Math.cos(argument)
-        ),
-      }
-    }, { X: ZERO, Y: ZERO, Z: ZERO })
-  }
-}
+import { getEccentricity, getLongitudeOfPerihelion } from '../coordinates'
+import { getEarthVelocity } from './earthvelocity'
 
 /**
  * Accurate annual aberration for equatorial coordinates
@@ -126,10 +30,10 @@ export function getEarthVelocity (jd: JulianDay | number, highPrecision: boolean
  * @memberof module:Earth
  */
 export function getAccurateAnnualEquatorialAberration (jd: JulianDay | number, coords: EquatorialCoordinates, highPrecision: boolean = true): EquatorialCoordinatesCorrection {
-  const ra = new Decimal(coords.rightAscension).degreesToRadians()
-  const dec = new Decimal(coords.declination).degreesToRadians()
-
   if (highPrecision) {
+    const ra = new Decimal(coords.rightAscension).degreesToRadians()
+    const dec = new Decimal(coords.declination).degreesToRadians()
+
     const cosAlpha = ra.cos()
     const sinAlpha = ra.sin()
     const cosDelta = dec.cos()
@@ -150,10 +54,14 @@ export function getAccurateAnnualEquatorialAberration (jd: JulianDay | number, c
       DeltaDeclination: MINUSONE.mul(Y).radiansToDegrees().mul(3600) // to obtain units of ArcSecond
     }
   } else {
-    const cosAlpha = Math.cos(ra.toNumber())
-    const sinAlpha = Math.sin(ra.toNumber())
-    const cosDelta = Math.cos(dec.toNumber())
-    const sinDelta = Math.sin(dec.toNumber())
+    const deg2rad = DEG2RAD.toNumber()
+    const ra = (Decimal.isDecimal(coords.rightAscension) ? coords.rightAscension.toNumber() : coords.rightAscension) * deg2rad
+    const dec = (Decimal.isDecimal(coords.declination) ? coords.declination.toNumber() : coords.declination) * deg2rad
+
+    const cosAlpha = Math.cos(ra)
+    const sinAlpha = Math.sin(ra)
+    const cosDelta = Math.cos(dec)
+    const sinDelta = Math.sin(dec)
 
     const velocity = getEarthVelocity(jd, highPrecision)
     const c = 17_314_463_350.0 // Speed of light in units of 10^-8 UA / day. See AA p 155.
@@ -186,19 +94,19 @@ export function getAccurateAnnualEquatorialAberration (jd: JulianDay | number, c
  * @memberof module:Earth
  */
 export function getAnnualEquatorialAberration (jd: JulianDay | number, coords: EquatorialCoordinates, highPrecision: boolean = true): EquatorialCoordinatesCorrection {
-  const ra: Radian = new Decimal(coords.rightAscension).degreesToRadians()
-  const dec: Radian = new Decimal(coords.declination).degreesToRadians()
-
-  const e: Decimal = getEccentricity(jd)
-  const k = CONSTANT_OF_ABERRATION
-
-  const pi: Radian = getLongitudeOfPerihelion(jd).degreesToRadians()
-  const epsilon = getTrueObliquityOfEcliptic(jd, highPrecision).degreesToRadians()
-  const sunLongitude: Radian = Sun.getGeometricEclipticLongitude(jd, highPrecision).degreesToRadians()
-
   let DeltaRightAscension: ArcSecond, DeltaDeclination: ArcSecond
 
   if (highPrecision) {
+    const ra: Radian = new Decimal(coords.rightAscension).degreesToRadians()
+    const dec: Radian = new Decimal(coords.declination).degreesToRadians()
+
+    const e: Decimal = getEccentricity(jd)
+    const k = CONSTANT_OF_ABERRATION
+
+    const pi: Radian = getLongitudeOfPerihelion(jd).degreesToRadians()
+    const epsilon = getTrueObliquityOfEcliptic(jd, highPrecision).degreesToRadians()
+    const sunLongitude: Radian = Sun.getGeometricEclipticLongitude(jd, highPrecision).degreesToRadians()
+
     const cosPi = pi.cos()
     const sinPi = pi.sin()
     const cosEpsilon = epsilon.cos()
@@ -223,32 +131,42 @@ export function getAnnualEquatorialAberration (jd: JulianDay | number, coords: E
     const D2 = cosPi.mul(cosEpsilon).mul(D0).plus(cosAlpha.mul(sinDelta).mul(sinPi))
     DeltaDeclination = (MINUSONE.mul(k).mul(D1)).plus(e.mul(k).mul(D2))
   } else {
-    const cosPi = Math.cos(pi.toNumber())
-    const sinPi = Math.sin(pi.toNumber())
-    const cosEpsilon = Math.cos(epsilon.toNumber())
-    const tanEpsilon = Math.tan(epsilon.toNumber())
+    const deg2rad = DEG2RAD.toNumber()
+    const ra = (Decimal.isDecimal(coords.rightAscension) ? coords.rightAscension.toNumber() : coords.rightAscension) * deg2rad
+    const dec = (Decimal.isDecimal(coords.declination) ? coords.declination.toNumber() : coords.declination) * deg2rad
+
+    const e = getEccentricity(jd).toNumber()
+    const k = CONSTANT_OF_ABERRATION.toNumber()
+
+    const pi = getLongitudeOfPerihelion(jd).toNumber() * deg2rad
+    const epsilon = getTrueObliquityOfEcliptic(jd, highPrecision).toNumber() * deg2rad
+    const sunLongitude = Sun.getGeometricEclipticLongitude(jd, highPrecision).toNumber() * deg2rad
+
+    const cosPi = Math.cos(pi)
+    const sinPi = Math.sin(pi)
+    const cosEpsilon = Math.cos(epsilon)
+    const tanEpsilon = Math.tan(epsilon)
 
     // Use the geoMETRIC longitude. See AA p.151
-    const cosLong = Math.cos(sunLongitude.toNumber())
-    const sinLong = Math.sin(sunLongitude.toNumber())
+    const cosLong = Math.cos(sunLongitude)
+    const sinLong = Math.sin(sunLongitude)
 
-    const cosAlpha = Math.cos(ra.toNumber())
-    const sinAlpha = Math.sin(ra.toNumber())
-    const cosDelta = Math.cos(dec.toNumber())
-    const sinDelta = Math.sin(dec.toNumber())
+    const cosAlpha = Math.cos(ra)
+    const sinAlpha = Math.sin(ra)
+    const cosDelta = Math.cos(dec)
+    const sinDelta = Math.sin(dec)
 
     const A0 = cosAlpha * cosLong * cosEpsilon + sinAlpha * sinLong
     const A1 = cosAlpha * cosPi * cosEpsilon + sinAlpha * sinPi
     DeltaRightAscension = new Decimal(
-      -1 * k.toNumber() * (A0 / cosDelta)
-      + e.toNumber() * k.toNumber() * A1 / cosDelta
+      -1 * k * (A0 / cosDelta) + e * k * A1 / cosDelta
     )
 
     const D0 = tanEpsilon * cosDelta - sinAlpha * sinDelta
     const D1 = cosLong * cosEpsilon * D0 + cosAlpha * sinDelta * sinLong
     const D2 = cosPi * cosEpsilon * D0 + cosAlpha * sinDelta * sinPi
     DeltaDeclination = new Decimal(
-      -1 * k.toNumber() * D1 + e.toNumber() * k.toNumber() * D2
+      -1 * k * D1 + e * k * D2
     )
   }
 
--- src/earth/aberration/index.ts
+++ src/earth/aberration/index.ts
@@ -1,10 +1,11 @@
 import {
-  getEarthVelocity,
   getAccurateAnnualEquatorialAberration,
   getAnnualEclipticAberration,
   getAnnualEquatorialAberration,
   getNutationEquatorialAberration
 } from './aberration'
+
+import { getEarthVelocity } from './earthvelocity'
 
 export {
   getEarthVelocity,
