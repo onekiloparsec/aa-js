dcac4f3ef88de596906a5d9910a08f19d8692de3
Cédric Foellmi <cedric@onekiloparsec.dev>
2023-11-18 19:24:10 +0100

    Cleanup index
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- src/index.ts
+++ src/index.ts
@@ -12,24 +12,22 @@ import { Jupiter, Mars, Mercury, Neptune, Pluto, Saturn, Uranus, Venus } from '.
 
 import * as coordinates from './coordinates'
 import * as cosmology from './cosmology'
-import * as dates from './dates'
 import * as distances from './distances'
 import * as exoplanets from './exoplanets'
 import * as juliandays from './juliandays'
-import * as precession from './precession'
 import * as sexagesimal from './sexagesimal'
 import * as risetransitset from './risetransitset'
 import * as times from './times'
 
-const Planets: { [K in PlanetName]: Planet } = {
+const Planets: { [key in PlanetName]: Planet } = {
   Mercury, Venus, Mars, Jupiter, Saturn, Uranus, Neptune
 }
 
-const AllPlanets: { [K in AllPlanetName]: Planet | MinorPlanet } = {
+const AllPlanets: { [key in AllPlanetName]: Planet | MinorPlanet } = {
   Mercury, Venus, Mars, Jupiter, Saturn, Uranus, Neptune, Pluto
 }
 
-const SolarSystem: { [K in SolarSystemName]: NaturalSun | Planet | EarthPlanet | MinorPlanet } = {
+const SolarSystem: { [key in SolarSystemName]: NaturalSun | Planet | EarthPlanet | MinorPlanet } = {
   Sun, Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune, Pluto
 }
 
@@ -50,11 +48,9 @@ export {
   SolarSystem,
   coordinates,
   cosmology,
-  dates,
   distances,
   exoplanets,
   juliandays,
-  precession,
   sexagesimal,
   risetransitset,
   times
