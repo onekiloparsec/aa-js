add1566ec370e3a77ec033845ccaf0c04885c6c3
Cédric Foellmi <cedric@onekiloparsec.dev>
2024-04-22 22:22:24 +0200

    Removing all Decimaljs references
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- src/coordinates/ecliptic2equatorial.ts
+++ src/coordinates/ecliptic2equatorial.ts
@@ -1,105 +1,65 @@
 /**
  @module Coordinates
  */
-import Decimal from '@/decimal'
-import { Degree, EclipticCoordinates, EclipticCoordinatesNum, EquatorialCoordinates } from '@/types'
+import { Degree, EclipticCoordinates, EquatorialCoordinates } from '@/types'
 import { DEG2RAD, ECLIPTIC_OBLIQUITY_J2000_0 } from '@/constants'
 import { fmod360, fmod90 } from '@/utils'
 
 /**
  * Equatorial right ascension from ecliptic coordinates
- * @param {EclipticCoordinates | EclipticCoordinatesNum} coords The ecliptic coordinates
+ * @param {EclipticCoordinates} coords The ecliptic coordinates
  * @param {Degree} epsilon The ecliptic obliquity (default = obliquity of J2000)
- * @param {boolean} highPrecision Use (slower) arbitrary-precision decimal computations. default = true.
  * @returns {Degree} Degree (v3.2+), not HOURS (< v3.2)
  */
-export function getRightAscensionFromEcliptic (coords: EclipticCoordinates | EclipticCoordinatesNum,
-                                               epsilon: Degree | number = ECLIPTIC_OBLIQUITY_J2000_0,
-                                               highPrecision: boolean = true): Degree {
-  if (highPrecision) {
-    const dcoords = {
-      longitude: new Decimal(coords.longitude).degreesToRadians(),
-      latitude: new Decimal(coords.latitude).degreesToRadians()
-    }
-    const depsilon = new Decimal(epsilon).degreesToRadians()
-    const value = Decimal.atan2(
-      Decimal.sin(dcoords.longitude).mul(Decimal.cos(depsilon))
-        .minus(Decimal.tan(dcoords.latitude).mul(Decimal.sin(depsilon))),
-      Decimal.cos(dcoords.longitude)
-    )
-    return fmod360(new Decimal(value).radiansToDegrees())
-  } else {
-    const deg2rad = DEG2RAD.toNumber()
-    const dcoords = {
-      longitude: (Decimal.isDecimal(coords.longitude) ? coords.longitude.toNumber() : coords.longitude) * deg2rad,
-      latitude: (Decimal.isDecimal(coords.latitude) ? coords.latitude.toNumber() : coords.latitude) * deg2rad,
-    }
-    const depsilon = (Decimal.isDecimal(epsilon) ? epsilon.toNumber() : epsilon) * deg2rad
-    const value = Math.atan2(
-      Math.sin(dcoords.longitude) * Math.cos(depsilon)
-      - Math.tan(dcoords.latitude) * Math.sin(depsilon),
-      Math.cos(dcoords.longitude)
-    )
-    return fmod360(value / deg2rad)
+export function getRightAscensionFromEcliptic (coords: EclipticCoordinates, epsilon: Degree | number = ECLIPTIC_OBLIQUITY_J2000_0): Degree {
+  const rcoords = {
+    longitude: coords.longitude * DEG2RAD,
+    latitude: coords.latitude * DEG2RAD
   }
+  const repsilon = epsilon * DEG2RAD
+  const value = Math.atan2(
+    Math.sin(rcoords.longitude) * Math.cos(repsilon) - Math.tan(rcoords.latitude) * Math.sin(repsilon),
+    Math.cos(rcoords.longitude)
+  )
+  return fmod360(value / DEG2RAD)
 }
 
 /**
  * Equatorial declination from ecliptic coordinates
- * @param {EclipticCoordinates | EclipticCoordinatesNum} coords The ecliptic coordinates
+ * @param {EclipticCoordinates} coords The ecliptic coordinates
  * @param {Degree} epsilon The obliquity of the ecliptic; that is, the angle between the ecliptic
  * and the celestial equator. The mean obliquity (epsilon0) is given by nutation.getMeanObliquityOfEcliptic(jd).
  * If however the *apparent* R.A. and Dec. are required (that is, affected by aberration and nutation), the
  * true obliquity epsilon + Delta epsilon should be used. One can use nutation.getTrueObliquityOfEcliptic(jd)
  * If R.A. and Dec. are referred to the standard equinox of J2000, epsilon must be that of ECLIPTIC_OBLIQUITY_J2000_0.
- * @param {boolean} highPrecision Use (slower) arbitrary-precision decimal computations. default = true.
  * @return {Degree}
  */
-export function getDeclinationFromEcliptic (coords: EclipticCoordinates | EclipticCoordinatesNum,
-                                            epsilon: Degree | number = ECLIPTIC_OBLIQUITY_J2000_0,
-                                            highPrecision: boolean = true): Degree {
-  if (highPrecision) {
-    const dcoords = {
-      longitude: new Decimal(coords.longitude).degreesToRadians(),
-      latitude: new Decimal(coords.latitude).degreesToRadians()
-    }
-    const depsilon = new Decimal(epsilon).degreesToRadians()
-    const value = Decimal.asin(
-      Decimal.sin(dcoords.latitude).mul(Decimal.cos(depsilon))
-        .plus(Decimal.cos(dcoords.latitude).mul(Decimal.sin(depsilon)).mul(Decimal.sin(dcoords.longitude)))
-    )
-    return fmod90(new Decimal(value).radiansToDegrees())
-  } else {
-    const deg2rad = DEG2RAD.toNumber()
-    const dcoords = {
-      longitude: (Decimal.isDecimal(coords.longitude) ? coords.longitude.toNumber() : coords.longitude) * deg2rad,
-      latitude: (Decimal.isDecimal(coords.latitude) ? coords.latitude.toNumber() : coords.latitude) * deg2rad,
-    }
-    const depsilon = (Decimal.isDecimal(epsilon) ? epsilon.toNumber() : epsilon) * deg2rad
-    const value = Math.asin(
-      Math.sin(dcoords.latitude) * Math.cos(depsilon)
-      + Math.cos(dcoords.latitude) * Math.sin(depsilon) * Math.sin(dcoords.longitude)
-    )
-    return fmod90(value / deg2rad)
+export function getDeclinationFromEcliptic (coords: EclipticCoordinates, epsilon: Degree | number = ECLIPTIC_OBLIQUITY_J2000_0): Degree {
+  const rcoords = {
+    longitude: coords.longitude * DEG2RAD,
+    latitude: coords.latitude * DEG2RAD
   }
+  const repsilon = epsilon * DEG2RAD
+  const value = Math.asin(
+    Math.sin(rcoords.latitude) * Math.cos(repsilon)
+    + Math.cos(rcoords.latitude) * Math.sin(repsilon) * Math.sin(rcoords.longitude)
+  )
+  return fmod90(value / DEG2RAD)
 }
 
 /**
  * Transform ecliptic longitude and latitude to equatorial coordinates.
- * @param {EclipticCoordinates | EclipticCoordinatesNum} coords The ecliptic coordinates
+ * @param {EclipticCoordinates} coords The ecliptic coordinates
  * @param {Degree} epsilon The obliquity of the ecliptic; that is, the angle between the ecliptic
  * and the celestial equator. The mean obliquity (epsilon0) is given by nutation.getMeanObliquityOfEcliptic(jd).
  * If however the *apparent* R.A. and Dec. are required (that is, affected by aberration and nutation), the
  * true obliquity epsilon + Delta epsilon should be used. One can use nutation.getTrueObliquityOfEcliptic(jd)
  * If R.A. and Dec. are referred to the standard equinox of J2000, epsilon must be that of ECLIPTIC_OBLIQUITY_J2000_0.
- * @param {boolean} highPrecision Use (slower) arbitrary-precision decimal computations. default = true.
  * @returns {EquatorialCoordinates}
  */
-export function transformEclipticToEquatorial (coords: EclipticCoordinates | EclipticCoordinatesNum,
-                                               epsilon: Degree | number = ECLIPTIC_OBLIQUITY_J2000_0,
-                                               highPrecision: boolean = true): EquatorialCoordinates {
+export function transformEclipticToEquatorial (coords: EclipticCoordinates, epsilon: Degree | number = ECLIPTIC_OBLIQUITY_J2000_0): EquatorialCoordinates {
   return {
-    rightAscension: getRightAscensionFromEcliptic(coords, epsilon, highPrecision),
-    declination: getDeclinationFromEcliptic(coords, epsilon, highPrecision)
+    rightAscension: getRightAscensionFromEcliptic(coords, epsilon),
+    declination: getDeclinationFromEcliptic(coords, epsilon)
   }
 }
--- src/coordinates/equatorial2ecliptic.ts
+++ src/coordinates/equatorial2ecliptic.ts
@@ -1,96 +1,70 @@
 /**
  @module Coordinates
  */
-import Decimal from '@/decimal'
-import { Degree, EclipticCoordinates, EquatorialCoordinates, EquatorialCoordinatesNum } from '@/types'
-import { ECLIPTIC_OBLIQUITY_J2000_0 } from '@/constants'
+import { Degree, EclipticCoordinates, EquatorialCoordinates } from '@/types'
+import { DEG2RAD, ECLIPTIC_OBLIQUITY_J2000_0, RAD2DEG } from '@/constants'
 import { fmod360, fmod90 } from '@/utils'
 
 
 /**
  * Ecliptic longitude from equatorial coordinates
- * @param {EquatorialCoordinates | EquatorialCoordinatesNum} coords The equatorial coordinates (in degrees)
+ * @param {EquatorialCoordinates} coords The equatorial coordinates (in degrees)
  * @param {Degree} epsilon The obliquity of the ecliptic; that is, the angle between the ecliptic
  * and the celestial equator. The mean obliquity (epsilon0) is given by nutation.getMeanObliquityOfEcliptic(jd).
  * If however the *apparent* R.A. and Dec. are required (that is, affected by aberration and nutation), the
  * true obliquity epsilon + Delta epsilon should be used. One can use nutation.getTrueObliquityOfEcliptic(jd)
  * If R.A. and Dec. are referred to the standard equinox of J2000, epsilon must be that of ECLIPTIC_OBLIQUITY_J2000_0.
- * @param {boolean} highPrecision Use (slower) arbitrary-precision decimal computations. default = true.
  * @returns {Degree}
  */
-export function getEclipticLongitudeFromEquatorial (coords: EquatorialCoordinates | EquatorialCoordinatesNum,
-                                                    epsilon: Degree | number = ECLIPTIC_OBLIQUITY_J2000_0,
-                                                    highPrecision: boolean = true): Degree {
-  const dcoords = {
-    rightAscension: new Decimal(coords.rightAscension).degreesToRadians(),
-    declination: new Decimal(coords.declination).degreesToRadians()
+export function getEclipticLongitudeFromEquatorial (coords: EquatorialCoordinates, epsilon: Degree | number = ECLIPTIC_OBLIQUITY_J2000_0): Degree {
+  const rcoords = {
+    rightAscension: coords.rightAscension * DEG2RAD,
+    declination: coords.declination * DEG2RAD
   }
-  const depsilon = new Decimal(epsilon).degreesToRadians()
-  let value
-  if (highPrecision) {
-    value = Decimal.atan2(
-      Decimal.sin(dcoords.rightAscension).mul(Decimal.cos(depsilon))
-        .plus(Decimal.tan(dcoords.declination).mul(Decimal.sin(depsilon))),
-      Decimal.cos(dcoords.rightAscension)
-    )
-  } else {
-    value = Math.atan2(
-      Math.sin(dcoords.rightAscension.toNumber()) * Math.cos(depsilon.toNumber())
-      + Math.tan(dcoords.declination.toNumber()) * Math.sin(depsilon.toNumber()),
-      Math.cos(dcoords.rightAscension.toNumber())
-    )
-  }
-  return fmod360(new Decimal(value).radiansToDegrees())
+  const depsilon = epsilon * DEG2RAD
+  const value = Math.atan2(
+    Math.sin(rcoords.rightAscension) * Math.cos(depsilon) + Math.tan(rcoords.declination) * Math.sin(depsilon),
+    Math.cos(rcoords.rightAscension)
+  )
+  return fmod360(value * RAD2DEG)
 }
 
 /**
  * Ecliptic latitude from equatorial coordinates
- * @param {EquatorialCoordinates | EquatorialCoordinatesNum} coords The equatorial coordinates (in degrees)
+ * @param {EquatorialCoordinates} coords The equatorial coordinates (in degrees)
  * @param {Degree} epsilon The obliquity of the ecliptic; that is, the angle between the ecliptic
  * and the celestial equator. The mean obliquity (epsilon0) is given by nutation.getMeanObliquityOfEcliptic(jd).
  * If however the *apparent* R.A. and Dec. are required (that is, affected by aberration and nutation), the
  * true obliquity epsilon + Delta epsilon should be used. One can use nutation.getTrueObliquityOfEcliptic(jd)
  * If R.A. and Dec. are referred to the standard equinox of J2000, epsilon must be that of ECLIPTIC_OBLIQUITY_J2000_0.
- * @param {boolean} highPrecision Use (slower) arbitrary-precision decimal computations. default = true. * @returns {Degree}
  */
-export function getEclipticLatitudeFromEquatorial (coords: EquatorialCoordinates | EquatorialCoordinatesNum,
-                                                   epsilon: Degree | number = ECLIPTIC_OBLIQUITY_J2000_0,
-                                                   highPrecision: boolean = true): Degree {
-  const dcoords = {
-    rightAscension: new Decimal(coords.rightAscension).degreesToRadians(),
-    declination: new Decimal(coords.declination).degreesToRadians()
+export function getEclipticLatitudeFromEquatorial (coords: EquatorialCoordinates, epsilon: Degree | number = ECLIPTIC_OBLIQUITY_J2000_0): Degree {
+  const rcoords = {
+    rightAscension: coords.rightAscension * DEG2RAD,
+    declination: coords.declination * DEG2RAD
   }
-  const depsilon = new Decimal(epsilon).degreesToRadians()
-  let value
-  if (highPrecision) {
-    value = Decimal.asin(
-      Decimal.sin(dcoords.declination).mul(Decimal.cos(depsilon))
-        .minus(Decimal.cos(dcoords.declination).mul(Decimal.sin(depsilon).mul(Decimal.sin(dcoords.rightAscension))))
-    )
-  } else {
-    value = Math.asin(
-      Math.sin(dcoords.declination.toNumber()) * Math.cos(depsilon.toNumber())
-      - Math.cos(dcoords.declination.toNumber()) * Math.sin(depsilon.toNumber()) * Math.sin(dcoords.rightAscension.toNumber())
-    )
-  }
-  return fmod90(new Decimal(value).radiansToDegrees())
+  const depsilon = epsilon * DEG2RAD
+  
+  const value = Math.asin(
+    Math.sin(rcoords.declination) * Math.cos(depsilon)
+    - Math.cos(rcoords.declination) * Math.sin(depsilon) * Math.sin(rcoords.rightAscension)
+  )
+  
+  return fmod90(value * RAD2DEG)
 }
 
 /**
  * Transform equatorial coordinates to ecliptic coordinates
- * @param {EquatorialCoordinates | EquatorialCoordinatesNum} coords The equatorial coordinates (in degrees)
+ * @param {EquatorialCoordinates} coords The equatorial coordinates (in degrees)
  * @param {Degree} epsilon The obliquity of the ecliptic; that is, the angle between the ecliptic
  * and the celestial equator. The mean obliquity (epsilon0) is given by nutation.getMeanObliquityOfEcliptic(jd).
  * If however the *apparent* R.A. and Dec. are required (that is, affected by aberration and nutation), the
  * true obliquity epsilon + Delta epsilon should be used. One can use nutation.getTrueObliquityOfEcliptic(jd)
  * If R.A. and Dec. are referred to the standard equinox of J2000, epsilon must be that of ECLIPTIC_OBLIQUITY_J2000_0.
- * @param {boolean} highPrecision Use (slower) arbitrary-precision decimal computations. default = true. * @returns {Degree}
  */
-export function transformEquatorialToEcliptic (coords: EquatorialCoordinates | EquatorialCoordinatesNum,
-                                               epsilon: Degree | number = ECLIPTIC_OBLIQUITY_J2000_0,
-                                               highPrecision: boolean = true): EclipticCoordinates {
+export function transformEquatorialToEcliptic (coords: EquatorialCoordinates, epsilon: Degree | number = ECLIPTIC_OBLIQUITY_J2000_0): EclipticCoordinates {
   return {
-    longitude: getEclipticLongitudeFromEquatorial(coords, epsilon, highPrecision),
-    latitude: getEclipticLatitudeFromEquatorial(coords, epsilon, highPrecision)
+    longitude: getEclipticLongitudeFromEquatorial(coords, epsilon),
+    latitude: getEclipticLatitudeFromEquatorial(coords, epsilon)
   }
 }
