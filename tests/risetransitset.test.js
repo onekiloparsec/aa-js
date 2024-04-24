dbe1e331adbaaeed010e4dec5913540fab56e130
Cédric Foellmi <cedric@onekiloparsec.dev>
2024-04-24 10:29:09 +0200

    New tests of rise transit sets for umbc
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- tests/risetransitset.test.js
+++ tests/risetransitset.test.js
@@ -53,23 +53,6 @@ describe('rise transit & sets', () => {
   })
 
   // See AA, pp 103 & 104
-  test('approximate Venus on 1988 March 20 at Boston [low precision]', () => {
-    const date = new Date(Date.UTC(1988, 2, 20, 0, 0, 0))
-    const jd = juliandays.getJulianDay(date)
-    const coordsBoston = { latitude: 42.3333, longitude: -71.0833 }
-    const coordsVenus = { rightAscension: 41.73129, declination: 18.44092 }
-    const results = getRiseTransitSetTimes(jd, coordsVenus, coordsBoston, STANDARD_ALTITUDE_STARS, false)
-    expect(results.transit.isCircumpolar).toBeFalsy()
-    expect(results.transit.isAboveHorizon).toBeTruthy()
-    expect(results.transit.isAboveAltitude).toBeTruthy()
-    expect(results.rise.utc).toBeCloseTo(24 * 0.51766, 1)
-    expect(results.transit.utc).toBeCloseTo(24 * 0.81980, 1)
-    expect(results.set.utc).toBeCloseTo(24 * 0.12130, 2)
-    expect(results.rise.julianDay < results.transit.julianDay).toBeTruthy()
-    expect(results.transit.julianDay < results.set.julianDay).toBeTruthy()
-  })
-
-  // See AA, pp 103 & 104
   test('accurate Venus on 1988 March 20 at Boston', () => {
     const date = new Date(Date.UTC(1988, 2, 20, 0, 0, 0))
     const jd = juliandays.getJulianDay(date)
@@ -113,6 +96,24 @@ describe('rise transit & sets', () => {
     // Note, months are 0-based, hence 9 = october.
     const jd0 = juliandays.getJulianDay(new Date(Date.UTC(2023, 9, 14, 0, 0)))
     const umbc = { longitude: -76.70978311382578, latitude: 39.25443537644697 }
+    const coords = Venus.getGeocentricEquatorialCoordinates(jd0)
+    const results = getRiseTransitSetTimes(jd0, coords, umbc)
+    // See https://www.arcsecond.io/observingsites/cac8b50e-5602-467d-a98f-59358ab29077
+    const offsetUTC = -18000 // seconds
+    const offsetDST = 3600 // seconds
+    const offsetHours = (offsetUTC + offsetDST) / 3600
+    expect(fmod(results.rise.utc + offsetHours, 24)).toBeCloseTo(3.48, 1)//Originally 3.43?
+    expect(fmod(results.transit.utc + offsetHours, 24)).toBeCloseTo(10.0, 1)
+    expect(fmod(results.set.utc + offsetHours, 24)).toBeCloseTo(16.52, 1)//Originally 16.47
+  })
+
+  // See also AA p 103 for inspiration
+  test('accurate Venus on 2023 October 14 at UMBC', () => {
+    // We should normally use the Topocentric coordinates of Venus, not the Geocentric ones.
+    // Yet the topocentric corrections are usually small (< 30").
+    // Note, months are 0-based, hence 9 = october.
+    const jd0 = juliandays.getJulianDay(new Date(Date.UTC(2023, 9, 14, 0, 0)))
+    const umbc = { longitude: -76.70978311382578, latitude: 39.25443537644697 }
     const coords0 = Venus.getGeocentricEquatorialCoordinates(jd0 - 1)
     const coords1 = Venus.getGeocentricEquatorialCoordinates(jd0)
     const coords2 = Venus.getGeocentricEquatorialCoordinates(jd0 + 1)
@@ -121,9 +122,9 @@ describe('rise transit & sets', () => {
     const offsetUTC = -18000 // seconds
     const offsetDST = 3600 // seconds
     const offsetHours = (offsetUTC + offsetDST) / 3600
-    expect(fmod(results.rise.utc + offsetHours, 24)).toBeCloseTo(3.43, 1)
+    expect(fmod(results.rise.utc + offsetHours, 24)).toBeCloseTo(3.48, 1)//Originally 3.43?
     expect(fmod(results.transit.utc + offsetHours, 24)).toBeCloseTo(10.0, 1)
-    expect(fmod(results.set.utc + offsetHours, 24)).toBeCloseTo(16.47, 1)
+    expect(fmod(results.set.utc + offsetHours, 24)).toBeCloseTo(16.52, 1)//Originally 16.47
   })
 
   test('arcsecond night slider unit test', () => {
